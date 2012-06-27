faa5e59d4fbd86131f4d83e1d92f2e3092f8e22e
Fabian Pichler <fabian.pichler@synventis.com>
2012-06-27 16:01:44 +0200

    fix npe in celSlideShowEffectAfterFinish if the overlay is closed during an effect

--- src/main/webapp/resources/celJS/celanim/slideshow.js
+++ src/main/webapp/resources/celJS/celanim/slideshow.js
@@ -367,23 +367,25 @@ var celSlideShowEffectAfterFinish = function(effect) {
   var slideConfig = celSlideShowConfig.get(effect.options.slideShowElemId);
   var fadeimgtemp = $(slideConfig.htmlId + '_tmpImg');
   var fadeimg = $(slideConfig.htmlId);
-  fadeimg.src = fadeimgtemp.src;
-  fadeimg.setStyle({
-    'top' : fadeimgtemp.getStyle('top'),
-    'left' : fadeimgtemp.getStyle('left')
-  });
-  fadeimg.show();
-  fadeimgtemp.hide();
-  $(slideConfig.htmlId).fire('celanim_slideshow:afterChangeImage', slideConfig);
-  if (slideShowHasNextImage(slideConfig)) {
-    removeImageSize(fadeimgtemp);
-    var isNewImage = !fadeimgtemp.src.endsWith(slideConfig.nextimgsrc);
-    fadeimgtemp.src = slideConfig.nextimgsrc;
-    if (isNewImage && celSlideShowIsRunning(slideConfig.htmlId)) {
-      scheduleChangeImage(slideConfig.htmlId);
+  if (fadeimg && fadeimgtemp) {
+    fadeimg.src = fadeimgtemp.src;
+    fadeimg.setStyle({
+      'top' : fadeimgtemp.getStyle('top'),
+      'left' : fadeimgtemp.getStyle('left')
+    });
+    fadeimg.show();
+    fadeimgtemp.hide();
+    $(slideConfig.htmlId).fire('celanim_slideshow:afterChangeImage', slideConfig);
+    if (slideShowHasNextImage(slideConfig)) {
+      removeImageSize(fadeimgtemp);
+      var isNewImage = !fadeimgtemp.src.endsWith(slideConfig.nextimgsrc);
+      fadeimgtemp.src = slideConfig.nextimgsrc;
+      if (isNewImage && celSlideShowIsRunning(slideConfig.htmlId)) {
+        scheduleChangeImage(slideConfig.htmlId);
+      }
     }
+    $(slideConfig.htmlId).removeClassName('celanim_isChanging');
   }
-  $(slideConfig.htmlId).removeClassName('celanim_isChanging');
 };
 
 var celSlideShowEffects = new Hash();
