87b171a3c4e4e5522db007572ada4865ff9e16b9
Fabian Pichler <fabian.pichler@synventis.com>
2012-07-15 20:11:58 +0200

    improve error-logging add logging for number of images loaded

--- src/main/webapp/resources/celRTE/plugins/celimage/js/imagepicker.js
+++ src/main/webapp/resources/celRTE/plugins/celimage/js/imagepicker.js
@@ -68,8 +68,8 @@ var pickerUploadFinshed = function(event) {
             var json = transport.responseText.evalJSON();
             loadAttachmentListCallback(json, false, true);
           } else if ((typeof console != 'undefined') 
-              && (typeof console.debug != 'undefined')) {
-            console.debug('loadSlideShowDataAsync: noJSON!!! ', transport.responseText);
+              && (typeof console.warn != 'undefined')) {
+            console.warn('pickerUploadFinshed: noJSON!!! ', transport.responseText);
           }
         }
       });
@@ -97,9 +97,14 @@ var loadAttachmentList = function(baseurl) {
           var json = transport.responseText.evalJSON();
           loadAttachmentListCallback(json, true, false);
           callbackFnkt(json.length == stepNumber, scroll);
+        if ((typeof console != 'undefined') && (typeof console.log != 'undefined')) {
+          console.log('loadAttachmentList: total loaded [',
+              $$('.imagePickerSource').size() ,'] finished? ',
+              (json.length == stepNumber));
+        }
         } else if ((typeof console != 'undefined') 
-            && (typeof console.debug != 'undefined')) {
-          console.debug('loadSlideShowDataAsync: noJSON!!! ', transport.responseText);
+            && (typeof console.warn != 'undefined')) {
+          console.warn('loadAttachmentList: noJSON!!! ', transport.responseText);
         }
       }
     });
