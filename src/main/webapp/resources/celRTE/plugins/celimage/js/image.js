7ae25ccfd1275df8f57118e54f38bc43ae54c4ab
Fabian Pichler <fabian.pichler@synventis.com>
2012-09-30 21:19:00 +0200

    fix forcing maxDimensions for resizing in preview

--- src/main/webapp/resources/celRTE/plugins/celimage/js/image.js
+++ src/main/webapp/resources/celRTE/plugins/celimage/js/image.js
@@ -11,46 +11,47 @@ var CelImageDialog = {
   },
 
   init : function(ed) {
+    var _me = this;
     var f = document.forms[0], nl = f.elements, ed = tinyMCEPopup.editor, dom = ed.dom, n = ed.selection.getNode();
 
     tinyMCEPopup.resizeToInnerSize();
-    this.fillClassList('class_list');
-    this.fillGalleryList('gallery_list');
-    this.fillEffectList('effect_list');
+    _me.fillClassList('class_list');
+    _me.fillGalleryList('gallery_list');
+    _me.fillEffectList('effect_list');
     TinyMCE_EditableSelects.init();
 
     if (n.nodeName == 'IMG') {
       nl.src.value = dom.getAttrib(n, 'src').replace(/\?.*/, '');
       nl.celwidth.value = dom.getAttrib(n, 'width');
       nl.celheight.value = dom.getAttrib(n, 'height');
-      nl.cropX.value = this.getAttrib(n, 'cropX');
-      nl.cropY.value = this.getAttrib(n, 'cropY');
-      nl.cropWidth.value = this.getAttrib(n, 'cropWidth');
-      nl.cropHeight.value = this.getAttrib(n, 'cropHeight');
+      nl.cropX.value = _me.getAttrib(n, 'cropX');
+      nl.cropY.value = _me.getAttrib(n, 'cropY');
+      nl.cropWidth.value = _me.getAttrib(n, 'cropWidth');
+      nl.cropHeight.value = _me.getAttrib(n, 'cropHeight');
       if((nl.cropX.value != '') && (nl.cropY.value != '') && (nl.cropWidth.value != '') 
           && (nl.cropHeight.value != '')) {
         nl.isCropped.value = '1';
       }
       nl.alt.value = dom.getAttrib(n, 'alt');
       nl.title.value = dom.getAttrib(n, 'title');
-      nl.marginTop.value = this.getAttrib(n, 'marginTop');
-      nl.marginLeft.value = this.getAttrib(n, 'marginLeft');
-      nl.marginBottom.value = this.getAttrib(n, 'marginBottom');
-      nl.marginRight.value = this.getAttrib(n, 'marginRight');
-      nl.border.value = this.getAttrib(n, 'border');
-      selectByValue(f, 'align', this.getAttrib(n, 'align'));
-      selectByValue(f, 'class_list', this.getAttrib(n, 'class'), true, true);
-      selectByValue(f, 'gallery_list', this.getAttrib(n, 'gallery'), true, true);
-      selectByValue(f, 'effect_list', this.getAttrib(n, 'effect'), true, true);
-      nl.hasSlideshow.checked = this.getAttrib(n, 'hasSlideshow');
-      nl.hasOverlay.checked = this.getAttrib(n, 'hasOverlay');
-      nl.hasCloseButton.checked = this.getAttrib(n, 'hasCloseButton');
-      nl.isSlideshowManualStart.checked = this.getAttrib(n, 'isSlideshowManualStart');
-      nl.isSlideshowRandomStart.checked = this.getAttrib(n, 'isSlideshowRandomStart');
-      nl.hasSlideshowAddNavigation.checked = this.getAttrib(n, 'hasSlideshowAddNavigation');
-      nl.delay.value = this.getAttrib(n, 'delay');
-      nl.overlayWidth.value = this.getAttrib(n, 'overlayWidth');
-      nl.overlayHeight.value = this.getAttrib(n, 'overlayHeight');
+      nl.marginTop.value = _me.getAttrib(n, 'marginTop');
+      nl.marginLeft.value = _me.getAttrib(n, 'marginLeft');
+      nl.marginBottom.value = _me.getAttrib(n, 'marginBottom');
+      nl.marginRight.value = _me.getAttrib(n, 'marginRight');
+      nl.border.value = _me.getAttrib(n, 'border');
+      selectByValue(f, 'align', _me.getAttrib(n, 'align'));
+      selectByValue(f, 'class_list', _me.getAttrib(n, 'class'), true, true);
+      selectByValue(f, 'gallery_list', _me.getAttrib(n, 'gallery'), true, true);
+      selectByValue(f, 'effect_list', _me.getAttrib(n, 'effect'), true, true);
+      nl.hasSlideshow.checked = _me.getAttrib(n, 'hasSlideshow');
+      nl.hasOverlay.checked = _me.getAttrib(n, 'hasOverlay');
+      nl.hasCloseButton.checked = _me.getAttrib(n, 'hasCloseButton');
+      nl.isSlideshowManualStart.checked = _me.getAttrib(n, 'isSlideshowManualStart');
+      nl.isSlideshowRandomStart.checked = _me.getAttrib(n, 'isSlideshowRandomStart');
+      nl.hasSlideshowAddNavigation.checked = _me.getAttrib(n, 'hasSlideshowAddNavigation');
+      nl.delay.value = _me.getAttrib(n, 'delay');
+      nl.overlayWidth.value = _me.getAttrib(n, 'overlayWidth');
+      nl.overlayHeight.value = _me.getAttrib(n, 'overlayHeight');
       nl.style.value = dom.getAttrib(n, 'style');
       nl.id.value = dom.getAttrib(n, 'id');
       nl.dir.value = dom.getAttrib(n, 'dir');
@@ -63,10 +64,10 @@ var CelImageDialog = {
       if (ed.settings.inline_styles) {
         // Move attribs to styles
         if (dom.getAttrib(n, 'align'))
-          this.updateStyle('align');
+          _me.updateStyle('align');
 
         if (dom.getAttrib(n, 'border'))
-          this.updateStyle('border');
+          _me.updateStyle('border');
       }
       $('imagePicker_tab').down('a').observe('click',
           imagePicker_pickerTabFirstClickHandler);
@@ -76,8 +77,8 @@ var CelImageDialog = {
     }
 
     $('resetMax').observe('click', function(event) {
-      $('celwidth').value = $('resetMaxLabel').innerHTML.replace(/\D*(\d*) x.*/g, '$1');
-      $('celheight').value = $('resetMaxLabel').innerHTML.replace(/.*x (\d*)\D*/g, '$1');
+      $('celwidth').value = _me._getMaxWidth();
+      $('celheight').value = _me._getMaxHeight();
       CelImageDialog.showPreviewImage(CelImageDialog.replaceCropInURL(
           CelImageDialog.addAutoResizeToURL($('previewImg').src, $('celwidth').value, 
           $('celheight').value)), $('celwidth').value, $('celheight').value, 1);
@@ -86,7 +87,7 @@ var CelImageDialog = {
 
     // If option enabled default constrain proportions to checked
     if (ed.getParam("advimage_constrain_proportions", true)) {
-      this.constrain = true;
+      _me.constrain = true;
     }
 
     if (ed.getParam("celanim_slideshow", false)) {
@@ -99,9 +100,17 @@ var CelImageDialog = {
       $('crop_tab').show();
     }
 
-    this.changeAppearance();
-    this.showPreviewImage(this.replaceCropInURL(this.addAutoResizeToURL(nl.src.value, 
+    _me.changeAppearance();
+    _me.showPreviewImage(_me.replaceCropInURL(_me.addAutoResizeToURL(nl.src.value, 
         nl.celwidth.value, nl.celheight.value)), nl.celwidth.value, nl.celheight.value, 1);
+  },
+
+  _getMaxWidth : function() {
+    return $('resetMaxLabel').innerHTML.replace(/\D*(\d*) x.*/g, '$1');
+  },
+
+  _getMaxHeight : function() {
+    return $('resetMaxLabel').innerHTML.replace(/.*x (\d*)\D*/g, '$1');
   },
 
   insert : function(file, title) {
@@ -566,13 +575,18 @@ var CelImageDialog = {
       return;
     }
 
-    if (f.celwidth.value == "")
-      return;
-
-    tp = (parseInt(f.celwidth.value) / parseInt(t.preloadImg.width)) * t.preloadImg.height;
-    f.celheight.value = Math.floor(tp + 0.5);
-    t.showPreviewImage(t.replaceCropInURL(t.addAutoResizeToURL(f.src.value, 
-        f.celwidth.value, f.celheight.value)), f.celwidth.value, f.celheight.value, 1);
+    var maxWidthValue = parseInt(t._getMaxWidth());
+    var newWidthValue = parseInt(f.celwidth.value);
+    if (isNaN(newWidthValue) || (newWidthValue >= maxWidthValue)
+        || (newWidthValue <= 0)) {
+      f.celwidth.value = maxWidthValue;
+      f.celheight.value = t._getMaxHeight();
+    } else {
+      tp = (newWidthValue / parseInt(t.preloadImg.width)) * t.preloadImg.height;
+      f.celheight.value = Math.floor(tp + 0.5);
+      t.showPreviewImage(t.replaceCropInURL(t.addAutoResizeToURL(f.src.value, 
+          f.celwidth.value, f.celheight.value)), f.celwidth.value, f.celheight.value, 1);
+    }
   },
 
   changeWidth : function() {
@@ -582,13 +596,18 @@ var CelImageDialog = {
       return;
     }
 
-    if (f.celheight.value == "")
-      return;
-
-    tp = (parseInt(f.celheight.value) / parseInt(t.preloadImg.height)) * t.preloadImg.width;
-    f.celwidth.value = Math.floor(tp + 0.5);
-    t.showPreviewImage(t.replaceCropInURL(t.addAutoResizeToURL(f.src.value, 
-        f.celwidth.value, f.celheight.value)), f.celwidth.value, f.celheight.value, 1);
+    var maxHeightValue = parseInt(t._getMaxHeight());
+    var newHeightValue = parseInt(f.celheight.value);
+    if (isNaN(newHeightValue) || (newHeightValue >= maxHeightValue)
+        || (newHeightValue <= 0)) {
+      f.celheight.value = maxHeightValue;
+      f.celwidth.value = t._getMaxWidth();
+    } else {
+      tp = (newHeightValue / parseInt(t.preloadImg.height)) * t.preloadImg.width;
+      f.celwidth.value = Math.floor(tp + 0.5);
+      t.showPreviewImage(t.replaceCropInURL(t.addAutoResizeToURL(f.src.value, 
+          f.celwidth.value, f.celheight.value)), f.celwidth.value, f.celheight.value, 1);
+    }
   },
 
   updateStyle : function(ty) {
