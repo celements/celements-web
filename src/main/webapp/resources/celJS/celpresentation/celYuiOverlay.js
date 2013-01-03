84a9de677f6db4e518d71e4e82c7aa5a35f79213
Fabian Pichler <fabian.pichler@synventis.com>
2013-01-03 19:36:55 +0100

    extend celYuiOverlay to allow overwriting default dialog configuration.

--- src/main/webapp/resources/celJS/celpresentation/celYuiOverlay.js
+++ src/main/webapp/resources/celJS/celpresentation/celYuiOverlay.js
@@ -22,9 +22,10 @@ if(typeof CELEMENTS.presentation=="undefined"){CELEMENTS.presentation={};};
 //////////////////////////////////////////////////////////////////////////////
 // Celements presentation overlay class
 //////////////////////////////////////////////////////////////////////////////
-  CELEMENTS.presentation.Overlay = function() {
+  CELEMENTS.presentation.Overlay = function(configObj) {
   // constructor
-  this._init();
+  configObj = configObj || {};
+  this._init(configObj);
 };
 
 var cpoInstance = null;
@@ -40,46 +41,50 @@ CELEMENTS.presentation.getOverlayObj = function() {
   var CPO = CELEMENTS.presentation.Overlay;
 
   CELEMENTS.presentation.Overlay.prototype = {
-      overlayDialog : null,
+      _overlayDialog : null,
+      _defaultConfig : {
+        "width" : dialogWidth, 
+        fixedcenter: true, 
+        visible: false, 
+        draggable: false, 
+        close: false, 
+        zindex: 4, 
+        modal:true,
+        monitorresize:false,
+        icon: YAHOO.widget.SimpleDialog.ICON_HELP, 
+        constraintoviewport: true
+      },
+      _dialogConfig : undefined,
 
-      _init : function() {
+      _init : function(configObj) {
         var _me = this;
+        _me._dialogConfig = $H(_me._defaultConfig).merge(configObj).toObject(); 
       },
 
       getOverlayDialog : function(width) {
         var _me = this;
         var dialogWidth = width || "300px";
-        if(!_me.overlayDialog) {
-          _me.overlayDialog = new YAHOO.widget.SimpleDialog("modal dialog", {
-            "width" : dialogWidth, 
-            fixedcenter: true, 
-            visible: false, 
-            draggable: false, 
-            close: false, 
-            zindex: 4, 
-            modal:true,
-            monitorresize:false,
-            icon: YAHOO.widget.SimpleDialog.ICON_HELP, 
-            constraintoviewport: true
-          } );
-          _me.overlayDialog.hideEvent.subscribe(function() {
+        if(!_me.__overlayDialog) {
+          _me._overlayDialog = new YAHOO.widget.SimpleDialog("modal dialog",
+              _me._dialogConfig);
+          _me._overlayDialog.hideEvent.subscribe(function() {
             var bodyElem = $$('body')[0];
             bodyElem.setStyle({ 'overflow' : 'auto' });
             bodyElem.fire('cel_yuiOverlay:hideEvent');
           });
         } else {
-          _me.overlayDialog.cfg.setProperty('width', dialogWidth);
+          _me._overlayDialog.cfg.setProperty('width', dialogWidth);
         }
         //add skin-div to get default yui-skin-sam layouting for the dialog
         var yuiSamSkinDiv = new Element('div'
           ).addClassName('yui-skin-sam'
           ).addClassName('cel-YuiOverlay');
         $(document.body).insert(yuiSamSkinDiv);
-        _me.overlayDialog.render(yuiSamSkinDiv);
-        _me.overlayDialog.setBody('<img style="margin-left: auto; margin-right:auto;"'
+        _me._overlayDialog.render(yuiSamSkinDiv);
+        _me._overlayDialog.setBody('<img style="margin-left: auto; margin-right:auto;"'
             + ' src="/skin/resources/celRes/ajax-loader.gif" />');
         $(document.body).fire('cel_yuiOverlay:afterRenderDialog');
-        return _me.overlayDialog;
+        return _me._overlayDialog;
       },
 
       showProgressDialog : function(headerText) {
