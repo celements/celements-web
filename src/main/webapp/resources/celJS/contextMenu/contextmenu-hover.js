48c3353da8266b93b77106a956da3b10e42c593d
Fabian Pichler <fabian.pichler@synventis.com>
2013-01-30 12:03:50 +0100

    reimplement contextmenu-hover with outliner

--- src/main/webapp/resources/celJS/contextMenu/contextmenu-hover.js
+++ src/main/webapp/resources/celJS/contextMenu/contextmenu-hover.js
@@ -1,45 +1,81 @@
 /**
  * Enhancing contextmenu with an hover on elements with a contextMenu.
  */
+if(typeof CELEMENTS=="undefined"){var CELEMENTS={};};
+if(typeof CELEMENTS.layout=="undefined"){CELEMENTS.layout={};};
 
-var oldHighlighted = null;
+(function(window, undefined) {
 
-var celementsHighlightCell = function(event) {
-  var onCell = event.findElement();
-  if (onCell && contextMenuItemDataForElemId.get(onCell.id)) {
-	if (oldHighlighted) {
-      celementsUNhighlightCell(oldHighlighted);
-      oldHighlighted = null;
-	}
-    if(event.shiftKey && !onCell.hasClassName('cm_highlighted')) {
-      onCell.addClassName('cm_highlighted');
-      onCell.stopObserving('mouseout', celementsCMHighlightMouseOut);
-      onCell.observe('mouseout', celementsCMHighlightMouseOut);
-      oldHighlighted = onCell;
-    }
-  }
-}
-var celementsCMHighlightMouseOut = function(event) {
-  var onCell = event.findElement();
-  if(onCell && onCell.hasClassName('cm_highlighted')) {
-    celementsUNhighlightCell(onCell);
-  }
-}
+  /**
+   * FIXME ContextMenuHover needs so far global access to 'contextMenuItemDataForElemId'
+   */
+  CELEMENTS.layout.ContextMenuHover = function(outliner) {
+    // constructor
+    this._init(outliner);
+  };
 
-var celementsUNhighlightCell = function(theCell) {
-  if(theCell.hasClassName('cm_highlighted')) {
-	  theCell.removeClassName('cm_highlighted');
-  }
-  theCell.stopObserving('mouseout', celementsCMHighlightMouseOut);
-}
+  (function() {
 
-var addContextMenuHover = function() {
-  contextMenuItemDataForElemId.keys().each(function(elemId) {
-    if($(elemId)) {
-      $(elemId).stopObserving('mouseover', celementsHighlightCell);
-      $(elemId).observe('mouseover', celementsHighlightCell);
-    }
-  });
-}
+    CELEMENTS.layout.ContextMenuHover.prototype = {
+      _outliner : null,
+      _oldHighlighted : null,
+      _CMHighlightMouseOutBinded : null,
+      _highlightCellBinded : null,
 
-Event.observe(window, 'celcontextmenu:loadingfinished', addContextMenuHover);
+      _init : function(outliner) {
+        var _me = this;
+        _me._outliner = outliner || new CELEMENTS.layout.Outliner();
+        _me._CMHighlightMouseOutBinded = _me._CMHighlightMouseOut.bind(_me);
+        _me._highlightCellBinded = _me._highlightCell.bind(_me);
+        Event.observe(window, 'celcontextmenu:loadingfinished',
+            _me.addContextMenuHover.bind(_me));
+      },
+
+      _highlightCell : function(event) {
+        var _me = this;
+        var onCell = event.findElement();
+        if (onCell && contextMenuItemDataForElemId.get(onCell.id)) {
+        if (_me._oldHighlighted) {
+          _me._UNhighlightCell(_me._oldHighlighted);
+          _me._oldHighlighted = null;
+        }
+          if(event.shiftKey && !onCell.hasClassName('cm_outline_highlighted')) {
+            onCell.addClassName('cm_outline_highlighted');
+            _me._outliner.outlineElement(onCell);
+            onCell.stopObserving('mouseout', _me._CMHighlightMouseOutBinded);
+            onCell.observe('mouseout', _me._CMHighlightMouseOutBinded);
+            _me._oldHighlighted = onCell;
+          }
+        }
+      },
+
+      _CMHighlightMouseOut : function(event) {
+        var _me = this;
+        var onCell = event.findElement();
+        if(onCell && onCell.hasClassName('cm_outline_highlighted')) {
+          _me._UNhighlightCell(onCell);
+        }
+      },
+    
+      _UNhighlightCell : function(theCell) {
+        var _me = this;
+        if(theCell.hasClassName('cm_outline_highlighted')) {
+          _me._outliner.removeOutlinesForElement(theCell);
+          theCell.removeClassName('cm_outline_highlighted');
+        }
+        theCell.stopObserving('mouseout', _me._CMHighlightMouseOutBinded);
+      },
+    
+      addContextMenuHover : function() {
+        contextMenuItemDataForElemId.keys().each(function(elemId) {
+          if($(elemId)) {
+            $(elemId).stopObserving('mouseover', _me._highlightCellBinded);
+            $(elemId).observe('mouseover', _me._highlightCellBinded);
+          }
+        });
+      }
+
+    };
+  })();
+
+})(window);
