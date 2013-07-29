d5ef241ec4fad8105e75d4ccb1dddc58e851dc5e
Fabian Pichler <fabian.pichler@synventis.com>
2013-07-29 14:19:05 +0200

    move FileBaseData to ajax scripts on disk

--- src/main/webapp/resources/celJS/initCelements.js
+++ src/main/webapp/resources/celJS/initCelements.js
@@ -3,3 +3,11 @@ var celOnBeforeLoadListenerArray = [];
 var celAddOnBeforeLoadListener = function(listenerFunc) {
   celOnBeforeLoadListenerArray.push(listenerFunc);
 };
+
+if (typeof getCelHost === 'undefined') {
+var getCelHost = function() {
+  var celHost = document.location + '?';
+  celHost = celHost.substring(0, celHost.indexOf('?'));
+  return celHost;
+};
+}
