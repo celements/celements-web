2a357870c64ad222f847180d7dc31d6470ae9333
Fabian Pichler <fabian.pichler@synventis.com>
2013-08-08 14:56:49 +0200

    add possibility to configure date format in DatePicker.js

--- src/main/webapp/resources/celJS/editorsupport/DatePicker.js
+++ src/main/webapp/resources/celJS/editorsupport/DatePicker.js
@@ -36,6 +36,7 @@ if(typeof CELEMENTS.editorsupport =="undefined"){CELEMENTS.editorsupport={};};
       _inputField : undefined,
       _pickerDateCal : undefined,
       _datePickerCon : undefined,
+      _selectedDateFormat : '',
 
       _init : function(inputField, datePickerIdNum) {
         var _me = this;
@@ -125,8 +126,6 @@ if(typeof CELEMENTS.editorsupport =="undefined"){CELEMENTS.editorsupport={};};
         var isOutside = (typeof event.findElement('#' + _me._datePickerCon.id
             ) == 'undefined');
         if (_me.visible() && isOutside) {
-          console.log("_calClickOutsideHandler: ", this, event.element(),
-              isOutside);
           Event.fire(_me._inputField, 'celements:datePicker-clickoutside', {
             'celCalDatePicker' : _me,
             'dateField' : _me._inputField
@@ -155,19 +154,45 @@ if(typeof CELEMENTS.editorsupport =="undefined"){CELEMENTS.editorsupport={};};
         _me._inputField.addClassName('celDatePickerVisible');
       },
 
+      setSelectedDateFormat : function(newFormatString) {
+        var _me = this;
+        if ($j.format) {
+          try {
+            //try if we can format a date with it
+            if (newFormatString != '') {
+              $j.format.date(new Date(), newFormatString);
+            }
+            _me._selectedDateFormat = newFormatString;
+          } catch (exp) {
+            if ((typeof console != 'undefined')
+                && (typeof console.error != 'undefined')) {
+              console.error('failed to parse new date format with: ' + newFormatString,
+                  exp);
+            }
+          }
+        } else if ((typeof console != 'undefined')
+            && (typeof console.warn != 'undefined')) {
+          console.warn('Failed to finde jquery-formater plugin!');
+        }
+      },
+
       _handleDateSelected : function(type, args) {
         var _me = this;
         var pickerDateCal = _me._pickerDateCal;
         var selDates = args[0];
         if ((selDates.length > 0) && _me._inputField) {
           var selDate = selDates[0];
+          var selectedDate = new Date(selDate[1] + '/' + selDate[2] + '/' + selDate[0]);
           var dateSelectedEvent = Event.fire(_me._inputField, 'celements:datePicker-dateSelected', {
             'selDate' : selDate,
-            'date' : new Date(selDate[1] + '/' + selDate[2] + '/' + selDate[0]),
+            'date' : selectedDate,
             'dateField' : _me._inputField
           });
           if (!dateSelectedEvent.stopped) {
             var newStartDate = selDate[2] + '.' + selDate[1] + '.' + selDate[0];
+            if ($j.format && (_me._selectedDateFormat != '')) {
+              newStartDate = $j.format.date(selectedDate, _me._selectedDateFormat);
+            }
             _me._inputField.value = newStartDate;
           }
         }
