a774801b2d5b2c82067c34cadafbec85981a9a1d
darikhasham <dari.khasham@synventis.com>
2014-07-15 16:44:20 +0200

    Added the DateTimePicker to the Rights Tab

--- src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
+++ src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
@@ -1,14 +1,22 @@
-/*
-var registerDatePickerListener = function() {
-	$$('.editObjField_Classes\\.DocumentPublication_publishDate input, .editObjField_Classes\\.DocumentPublication_unpublishDate input').each(function(inputfield) {
-	addDatePickerToField(inputfield);
-	var navDateCal = getDatePickerForInputField(inputfield);
-	navDateCal.setSelectedDateFormat("dd.MM.yyyy HH:mm");
-	});
-};
-
-$j(document).ready(function() {
-	registerDatePickerListener();
+jQuery("input").each(function( index, value ) {
+	var prefix = value.id.substr(0, value.id.indexOf('_'));
+	if(prefix == "Classes.DocumentPublication") {
+		jQuery("#"+value.id.replace(".", "\\.")).datetimepicker({	     
+			lang:'de', 
+			i18n:{
+				  de:{
+				   months:[
+				    'Januar','Februar','März','April',
+				    'Mai','Juni','Juli','August',
+				    'September','Oktober','November','Dezember',
+				   ],
+				   dayOfWeek:[
+				    "So", "Mo", "Di", "Mi", 
+				    "Do", "Fr", "Sa",
+				   ]
+				  }
+				 },
+			dayOfWeekStart: 1,
+			format:'d.m.Y H:i'});
+	}
 });
-*/
-jQuery('#Classes\\.DocumentPublication_0_publishDate').datetimepicker({format:'d.m.Y H:i'});
