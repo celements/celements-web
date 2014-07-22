7bf2f7953f754dd478721fe4547280128dfe66fd
darikhasham <dari.khasham@synventis.com>
2014-07-22 12:55:08 +0200

    Changes from the Pullrequest TODO: Multilingual for the DateTimePicker

--- src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
+++ src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
@@ -2,20 +2,7 @@ jQuery("input").each(function( index, value ) {
 	var prefix = value.id.substr(0, value.id.indexOf('_'));
 	if(prefix == "Classes.DocumentPublication") {
 		jQuery("#"+value.id.replace(".", "\\.")).datetimepicker({	     
-			lang:'de', 
-			i18n:{
-				  de:{
-				   months:[
-				    'Januar','Februar','März','April',
-				    'Mai','Juni','Juli','August',
-				    'September','Oktober','November','Dezember',
-				   ],
-				   dayOfWeek:[
-				    "So", "Mo", "Di", "Mi", 
-				    "Do", "Fr", "Sa",
-				   ]
-				  }
-				 },
+			lang:'de',
 			dayOfWeekStart: 1,
 			format:'d.m.Y H:i'});
 	}
