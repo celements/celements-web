395e35a61b6e414f200f07032d6c13da5c1f7f14
darikhasham <dari.khasham@synventis.com>
2014-07-25 18:31:42 +0200

    added multilingual

--- src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
+++ src/main/webapp/resources/celJS/celTabMenu/publishDatePicker.js
@@ -2,7 +2,7 @@ jQuery("input").each(function( index, value ) {
 	var prefix = value.id.substr(0, value.id.indexOf('_'));
 	if(prefix == "Classes.DocumentPublication") {
 		jQuery("#"+value.id.replace(".", "\\.")).datetimepicker({	     
-			lang:'de',
+			lang:Validation.messages.get("admin-language"),
 			dayOfWeekStart: 1,
 			format:'d.m.Y H:i'});
 	}
