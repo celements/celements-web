c29691f2bbb196afba798049ab485524c8e25c7b
Fabian Pichler <fabian.pichler@synventis.com>
2014-08-15 10:07:11 +0200

    handle htmlElem not found in _init of endlessScroller.js

--- src/main/webapp/resources/celJS/endlessScroll/endlessScroller.js
+++ src/main/webapp/resources/celJS/endlessScroll/endlessScroller.js
@@ -38,88 +38,92 @@ if(typeof CELEMENTS=="undefined"){var CELEMENTS={};};
 if(typeof CELEMENTS.anim=="undefined"){CELEMENTS.anim={};};
 var scrollAnims = new Hash();
 
-(function() {
-CELEMENTS.anim.EndlessScroll = function(id, action, params) {
-  // constructor
-  this._init(id, action, params);
-};
+(function(window, undefined) {
+  "use strict";
 
-var CES = CELEMENTS.anim.EndlessScroll;
+  CELEMENTS.anim.EndlessScroll = function(id, action, params) {
+    // constructor
+    this._init(id, action, params);
+  };
 
-CELEMENTS.anim.EndlessScroll.prototype = {
-  htmlElem : undefined,
-  action : undefined,
-  overlap : 0,
-  isScrollBlockEle : undefined,
-  loadAllOnInit : false,
-  _elementHeight : 0,
-  _observer : undefined,
-  _isLoading : false,
+  var CES = CELEMENTS.anim.EndlessScroll;
   
-  _init : function(elemId, action, params) {
-    var _me = this;
-    _me.htmlElem = $(elemId);
-    _me._elementHeight = Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight());
-    _me.action = action;
-    _me.loadAllOnInit = (typeof(params) != 'undefined') 
-        && (typeof(params.loadAllOnInit) != 'undefined') && params.loadAllOnInit;
-    if(typeof(params) != 'undefined') {
-      if(typeof(params.overlap) != 'undefined') {
-        _me.overlap = params.overlap;
-      }
-      if(typeof(params.isScrollBlockEle) != 'undefined') {
-        _me.isScrollBlockEle = params.isScrollBlockEle;
-      } else {
-        var elemOverflow = _me.htmlElem.getStyle('overflow-y');
-        if(elemOverflow == 'visible') {
-          _me.isScrollBlockEle = false;
+  CELEMENTS.anim.EndlessScroll.prototype = {
+    htmlElem : undefined,
+    action : undefined,
+    overlap : 0,
+    isScrollBlockEle : undefined,
+    loadAllOnInit : false,
+    _elementHeight : 0,
+    _observer : undefined,
+    _isLoading : false,
+    
+    _init : function(elemId, action, params) {
+      var _me = this;
+      _me.htmlElem = $(elemId);
+      if (_me.htmlElem) {
+        _me._elementHeight = Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight());
+        _me.action = action;
+        _me.loadAllOnInit = (typeof(params) != 'undefined') 
+            && (typeof(params.loadAllOnInit) != 'undefined') && params.loadAllOnInit;
+        if(typeof(params) != 'undefined') {
+          if(typeof(params.overlap) != 'undefined') {
+            _me.overlap = params.overlap;
+          }
+          if(typeof(params.isScrollBlockEle) != 'undefined') {
+            _me.isScrollBlockEle = params.isScrollBlockEle;
+          } else {
+            var elemOverflow = _me.htmlElem.getStyle('overflow-y');
+            if(elemOverflow == 'visible') {
+              _me.isScrollBlockEle = false;
+            } else {
+              _me.isScrollBlockEle = true;
+            }
+          }
+        }
+        if(((typeof(params) == 'undefined') || (typeof(params.executeOnInit) == 'undefined') 
+            || params.executeOnInit) || _me.loadAllOnInit) {
+          _me.isLoading = true;
+          _me.action(_me.htmlElem, _me.reloadDoneCallback);
+        }
+        if(_me.isScrollBlockEle) {
+          _me._observer = _me.htmlElem.observe('scroll', _me._checkIsScrollBottom);
         } else {
-          _me.isScrollBlockEle = true;
+          _me._observer = Element.observe(window, 'scroll', _me._checkIsScrollBottom);
+        }
+        scrollAnims.set(_me.htmlElem, _me);
+      }
+    },
+    
+    _checkIsScrollBottom : function(event) {
+      var _me = scrollAnims.get(event.element());
+      var pos = 0;
+      if(_me.isScrollBlockEle) {
+        pos = _me.htmlElem.scrollTop + _me.htmlElem.getHeight() - _me.htmlElem.scrollHeight;
+      } else {
+        -1*_me.htmlElem.viewportOffset().top + window.innerHeight - _me.htmlElem.scrollHeight;
+      }
+      if((pos + _me.overlap) >= 0) {
+        _me.isLoading = true;
+        _me.action(_me.htmlElem, _me.reloadDoneCallback);
+      }
+    },
+    
+    reloadDoneCallback : function(keepObserving, _me) {
+      if(keepObserving || (((typeof(keepObserving) == 'undefined') || (keepObserving == null)) && (_me._elementHeight < Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight())))) {
+        if(_me.loadAllOnInit || (keepObserving && (_me._elementHeight - _me.overlap <= _me.htmlElem.getHeight()))) {
+          _me.action(_me.htmlElem, _me.reloadDoneCallback);
+        } else {
+          _me.isLoading = false;
+        }
+      } else {
+        if(_me.isScrollBlockEle) {
+          _me.htmlElem.stopObserving('scroll');
+        } else {
+          window.stopObserving('scroll');
         }
       }
+      _me._elementHeight = Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight());
     }
-    if(((typeof(params) == 'undefined') || (typeof(params.executeOnInit) == 'undefined') 
-        || params.executeOnInit) || _me.loadAllOnInit) {
-      _me.isLoading = true;
-      _me.action(_me.htmlElem, _me.reloadDoneCallback);
-    }
-    if(_me.isScrollBlockEle) {
-      _me._observer = _me.htmlElem.observe('scroll', _me._checkIsScrollBottom);
-    } else {
-      _me._observer = Element.observe(window, 'scroll', _me._checkIsScrollBottom);
-    }
-    scrollAnims.set(_me.htmlElem, _me);
-  },
-  
-  _checkIsScrollBottom : function(event) {
-    var _me = scrollAnims.get(event.element());
-    var pos = 0;
-    if(_me.isScrollBlockEle) {
-      pos = _me.htmlElem.scrollTop + _me.htmlElem.getHeight() - _me.htmlElem.scrollHeight;
-    } else {
-      -1*_me.htmlElem.viewportOffset().top + window.innerHeight - _me.htmlElem.scrollHeight;
-    }
-    if((pos + _me.overlap) >= 0) {
-      _me.isLoading = true;
-      _me.action(_me.htmlElem, _me.reloadDoneCallback);
-    }
-  },
-  
-  reloadDoneCallback : function(keepObserving, _me) {
-    if(keepObserving || (((typeof(keepObserving) == 'undefined') || (keepObserving == null)) && (_me._elementHeight < Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight())))) {
-      if(_me.loadAllOnInit || (keepObserving && (_me._elementHeight - _me.overlap <= _me.htmlElem.getHeight()))) {
-        _me.action(_me.htmlElem, _me.reloadDoneCallback);
-      } else {
-        _me.isLoading = false;
-      }
-    } else {
-      if(_me.isScrollBlockEle) {
-        _me.htmlElem.stopObserving('scroll');
-      } else {
-        window.stopObserving('scroll');
-      }
-    }
-    _me._elementHeight = Math.max(_me.htmlElem.scrollHeight, _me.htmlElem.getHeight());
-  }
-};
-})();
+  };
+})(window);
