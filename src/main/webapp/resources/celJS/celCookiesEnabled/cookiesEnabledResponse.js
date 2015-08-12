855b705a37028b85f811f5720eebde64bb07d1c5
ebeutler <edoardo.beutler@synventis.com>
2015-08-12 18:03:07 +0200

    refactored CelCookieEnabledTest

---
+++ src/main/webapp/resources/celJS/celCookiesEnabled/cookiesEnabledResponse.js
@@ -0,0 +1,4 @@
+(function(window, undefined) {
+  var msgDoc = window.location.search.replace(/^.*[&\?]msgDoc=(.*?)(&.*)?$/g, '$1');
+  document.cookie = 'checkCookiesEnambledResponse=' + msgDoc;
+})(window);
