b99ddb0a7c347c944814db639577173e6b783774
ebeutler <edoardo.beutler@synventis.com>
2015-08-28 14:29:09 +0200

    improve logging

--- src/main/webapp/resources/celJS/celCookiesEnabled/CookiesEnabled.js
+++ src/main/webapp/resources/celJS/celCookiesEnabled/CookiesEnabled.js
@@ -88,7 +88,9 @@ if(typeof CELEMENTS.cookie=="undefined"){CELEMENTS.cookie={};};
         var cookiesEnabled = response.data.thirdPartyCookiesEnabled;
         if(!cookiesEnabled) { // IE Fix
           console.log('IE response.data properties:', response.data['thirdPartyCookiesEnabled']);
-          for(var propertyName in response.data) { console.log(propertyName); }
+//          for(var propertyName in response.data) { console.log(propertyName); }
+          console.log('toArray ', response.data.toArray());
+          console.log('evalJSON  ', response.data.evalJSON().thirdPartyCookiesEnabled);
         }
         console.log('response ', response);
         console.log('response data ', response.data);
