9a4e5b0cd15ded375f2f828dccaaf254bd5c92d0
ebeutler <edoardo.beutler@synventis.com>
2015-08-28 09:18:48 +0200

    add code to test postMessage solution

--- src/main/webapp/resources/celJS/celCookiesEnabled/cookiesEnabledQuery.js
+++ src/main/webapp/resources/celJS/celCookiesEnabled/cookiesEnabledQuery.js
@@ -1,6 +1,24 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 (function(window, undefined) {
   var responseDomain = window.location.search.replace(/^.*[&\?]domain=(.*?)(&.*)?$/g, '$1');
-//  var msgDoc = window.location.search.replace(/^.*[&\?]msgDoc=(.*?)(&.*)?$/g, '$1');
   var cookiesAllowed = 'no';
   var cookieName = "CelementsTestThirdPartyCookieEnabled";
   document.cookie = cookieName + "=yes;";
@@ -15,6 +33,9 @@
   if(locSearch.match(/[&\?]domain=http/)) {
     var loc = locSearch.replace(/^.*[&\?]domain=(.*?)(&.*)?$/g, '$1');
     var decodedLoc = decodeURIComponent(loc);
+    alert('decodedLoc = ' + decodedLoc);
+    parent.postMessage('message from beyond the iframe-veil', decodedLoc);
+    
     if(decodedLoc.indexOf('?') > -1) {
       decodedLoc += '&';
     } else {
