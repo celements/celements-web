9ba4d6e4f83108b6765dfcf33e6f85f7ed61aa38
Fabian Pichler <fabian.pichler@synventis.com>
2015-11-24 18:36:25 +0100

    add celanim_audio support to initOverlayLinksInsideParent

--- src/main/webapp/resources/celJS/celanim/movieplayer.js
+++ src/main/webapp/resources/celJS/celanim/movieplayer.js
@@ -440,8 +440,9 @@ var initOverlayLinks = function(flowclassname) {
 var initOverlayLinksInsideParent = function(parentElem, flowclassname) {
   if (parentElem.select(flowclassname).size() > 0) {
     parentElem.select(flowclassname).each(function(flowLink) {
-      if(flowLink.hasClassName('celanim_sfaudio')) {
-        flowLink.observe('click', celanimOpenInOverlaySFAudio);
+      if(flowLink.hasClassName('celanim_sfaudio')
+          || flowLink.hasClassName('celanim_audio')) {
+        flowLink.observe('click', celanimOpenInOverlayAudio);
       } else {
         flowLink.observe('click', celanimOpenInOverlay);
       }
@@ -456,7 +457,7 @@ var getCelHost = function() {
   return celHost;
 };
 
-var celanimOpenInOverlaySFAudio = function(e) {
+var celanimOpenInOverlayAudio = function(e) {
   celanimOpenInOverlay(e, 580, 70);
 };
 
