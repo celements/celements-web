54160adcfd5d560d75aabc3b275b605e942fbf66
Dari Khasham <dari.khasham@synventis.com>
2016-04-11 17:15:18 +0200

    extract the method of generate Multiselect to an EventListener

--- src/main/webapp/resources/celJS/initCelements.js
+++ src/main/webapp/resources/celJS/initCelements.js
@@ -697,20 +697,30 @@
   /**
    * Register all Bootstrap-Multiselect
    */
-  var selectedMultiselect = [];
-  celAddOnBeforeLoadListener(function() {
+  var cel_initAllMultiselect = function() {
     if($j().multiselect != undefined) {
       $j('.celMultiselect').multiselect({
         onDropdownHidden : function(event) {
           var element = event.target;
+          /* 
+           * FIXME: In Celements-framework the Multiselect disappears when the dropdown switched to hidden
+           * this code is just a workaround, it set the box visible again
+           */
           var jElement = $j(event.target);
-          jElement.css('display', '');        
+          jElement.css('display', '');
+          /*
           element.previous().select('option:selected').each(function(ele) {
             console.log('initCelements > onDropdownHidden > selected value: ', ele.value);
           });
+          */
         }
       });
     }
+  };
+  celAddOnBeforeLoadListener(function() {
+    $(document.body).stopObserving("cel:initMultiselect", cel_initAllMultiselect);
+    $(document.body).observe("cel:initMultiselect", cel_initAllMultiselect);
+    $(document.body).fire('cel:initMultiselect');
   });
 
 })(window);
