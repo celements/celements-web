4ffc4c5f47299e7fb63c0b43f28c7827fe1c8cbf
Dari Khasham <dari.khasham@synventis.com>
2017-08-15 17:33:05 +0200

    changes from PR

---
+++ src/main/webapp/resources/celJS/supportLink/supportLink.js
@@ -0,0 +1,32 @@
+(function(window, undefined) {
+  "use strict";
+  
+  /**
+   * Initialize all Jira-SupportBox
+   */
+  celAddOnBeforeLoadListener(function() {
+    openJiraSupportBoxInit();
+    window.ATL_JQ_PAGE_PROPS =  {
+      "triggerFunction": triggerFunction
+    };
+  });
+  
+  var triggerFunction = function(showCollectorDialog) {
+    //Requires that jQuery is available!
+    jQuery(".celements_menu_bar_support_item").click(function(e) {
+      e.preventDefault();
+      showCollectorDialog();
+    });
+  };
+
+  var openJiraSupportBoxInit = function() {
+    // Requires jQuery!
+    jQuery.ajax({
+        url: Validation.messages.get("supportLinkURL"),
+        type: "get",
+        cache: true,
+        dataType: "script"
+    });
+  };
+
+})(window);
