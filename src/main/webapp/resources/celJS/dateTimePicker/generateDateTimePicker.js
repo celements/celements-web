46d12e99d8adfc9a0be8a8e4afa91dabb775b04c
Fabian <fabian.pichler@synventis.com>
2021-03-08 16:23:03 +0100

    CELDEV-943 - JavaScript Web-Component for cel-date-time custom element (#250)
    * update jquery.datetimepicker.js to 2.5.20
    
    * remove "min" for datetimepicker.js
    
    * replace datetimepicker with full js
    
    * add logging
    
    * add logging
    
    * add logging in forEachAncestorOf
    
    * add logging in forEachAncestorOf
    
    * debugging
    
    * add ShadowSupport to forEachAncestorOf function
    
    * debug positioning
    
    * debugging
    
    * test shadow awarness in ancestorNodePosition computation
    
    * debugging
    
    * debugging
    
    * remove comment
    
    * reduce forEachAncestorOf shadowRoot patch
    
    * add jquery 1.12.4 dependency orig files
    
    * rename jquery.js to jquery.min.js
    
    * add jquery 1.12.4
    
    * logging
    
    * refix forEachAncestorOf for ShadowRoot elements
    
    * logging
    
    * logging
    
    * logging
    
    * move debugging to packaged jquery.js
    
    * logging
    
    * debugging
    
    * add jquery shadow dom fix
    
    * correct gBCR check
    
    * remove console.log
    
    * remove console.log in datetimepicker
    
    * update and add minified js versions
    
    * change to minified js versions
    
    * remove file-action from css loading
    
    * add i18n support for picker 2.5
    
    * logging
    
    * use typeof for setLocale defined check
    
    * move setLocale to init datetimepicker
    
    * remov minified for datetimepicker
    
    * add setLocal on init datetimepicker if in options
    
    * rebuild datetimepicker minified
    
    * change celements-web to load datetimepicker minified
    
    * use Class.create in generateDateTimePicker, add double init prevention
    
    * add Shadow-DOM Patch comment to jquery

--- src/main/webapp/resources/celJS/dateTimePicker/generateDateTimePicker.js
+++ src/main/webapp/resources/celJS/dateTimePicker/generateDateTimePicker.js
@@ -1,75 +1,68 @@
-(function(window, undefined) {
+(function (window, undefined) {
   "use strict";
 
-  if (typeof window.CELEMENTS == "undefined") { window.CELEMENTS={};};
-  if (typeof window.CELEMENTS.DATETIMEPICKER == "undefined") { window.CELEMENTS.DATETIMEPICKER={};};
+  if (typeof window.CELEMENTS == "undefined") { window.CELEMENTS = {}; }
+  if (typeof window.CELEMENTS.DATETIMEPICKER == "undefined") { window.CELEMENTS.DATETIMEPICKER = {}; }
 
-  var cel_initDateTimePicker = function(event) {
-    window.CELEMENTS.dateTimePickerGenerator.generateDateTimePicker();
-    $(document.body).stopObserving("celements:contentChanged", cel_initDateTimePicker);
-    $(document.body).observe("celements:contentChanged", cel_initDateTimePicker);
-  };
+  if (typeof window.CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator === 'undefined') {
+    const cel_initDateTimePicker = function (event) {
+      window.CELEMENTS.dateTimePickerGenerator.generateDateTimePicker();
+      $(document.body).stopObserving("celements:contentChanged", cel_initDateTimePicker);
+      $(document.body).observe("celements:contentChanged", cel_initDateTimePicker);
+    };
 
-  /**
-   * Initialize all DateTimePicker
-   */
-  celAddOnBeforeLoadListener(function() {
-    window.CELEMENTS.dateTimePickerGenerator = new CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator("body");
-    cel_initDateTimePicker();
-  });
+    /**
+     * Initialize all DateTimePicker
+     */
+    celAddOnBeforeLoadListener(function () {
+      window.CELEMENTS.dateTimePickerGenerator = new CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator("body");
+      cel_initDateTimePicker();
+    });
 
-  /**
-   * CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator constructor
-   */
-  CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator = function(htmlElementName) {
-    // constructor
-    this._init(htmlElementName);
-  };
+    /**
+     * class CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator definition
+     */
+    window.CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator = Class.create({
+      _htmlElement: undefined,
+      _onChangeEventBind: undefined,
 
-  /**
-   * class CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator definition
-   */
-  CELEMENTS.DATETIMEPICKER.DateTimePickerGenerator.prototype = {
-      _htmlElement : undefined,
-      _onChangeEventBind :undefined,
-
-      _init : function(htmlCssSelector) {
-        var _me = this;
+      initialize: function (htmlCssSelector) {
+        const _me = this;
         _me._htmlElement = $j(htmlCssSelector);
         _me._onChangeEventBind = _me._onChangeEvent.bind(_me);
       },
 
-      _onChangeEvent : function(event) {
-        var _me = this;
+      _onChangeEvent: function (event) {
+        const _me = this;
         _me._onChangeDateTime(event.element().value, event.element());
       },
 
-      _onChangeDateTime : function(currentValue, element){
+      _onChangeDateTime: function (currentValue, element) {
         if (element && element.length > 0) {
-          $(element[0]).fire('celForm:valueChanged', { 'currentValue' : currentValue });
+          $(element[0]).fire('celForm:valueChanged', { 'currentValue': currentValue });
         } else {
           console.debug('unable to fire celForm:valueChanged on', element);
         }
       },
 
-      generateDateTimePicker : function() {
-        var _me = this;
+      generateDateTimePicker: function () {
+        const _me = this;
         /**
          * Sample with additional Attribute cel_datePicker:
          * <input type="text" class="cel_datePicker" data-pickerAttr='{"format" : "Y.m.d", "minDate" : 0}'>
          */
-        _me._htmlElement.find('input.cel_datePicker').each(function(key, element){
-          var pickerAttrObj = {
-              // FIXME [CELDEV-904] DateTimePicker Language timing issue
-              'lang' : Validation.messages.get("admin-language") || 'de',
-              'dayOfWeekStart' : 1,
-              'format' : 'd.m.Y',
-              'timepicker' : false,
-              'closeOnDateSelect' : true,
-              'onChangeDateTime' : _me._onChangeDateTime
+        _me._htmlElement.find('input.cel_datePicker').each(function (key, element) {
+          let pickerAttrObj = {
+            // FIXME [CELDEV-904] DateTimePicker Language timing issue
+            'lang': Validation.messages.get("admin-language") || 'de',
+            'dayOfWeekStart': 1,
+            'format': 'd.m.Y',
+            'timepicker': false,
+            'closeOnDateSelect': true,
+            'onChangeDateTime': _me._onChangeDateTime
           };
-          var pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
-          if(pickerDataAttrObj) {
+          const pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
+          if (pickerDataAttrObj) {
             pickerAttrObj = $j.extend(pickerAttrObj, pickerDataAttrObj);
           }
           $j(element).datetimepicker(pickerAttrObj);
@@ -80,16 +73,16 @@
          * Sample with additional Attribute cel_timePicker:
          * <input type="text" class="cel_timePicker" data-pickerAttr='{"format":"i:H"}'>
          */
-        _me._htmlElement.find('input.cel_timePicker').each(function(key, element){
-          var pickerAttrObj = {
-              // FIXME [CELDEV-904] DateTimePicker Language timing issue
-              'lang' : Validation.messages.get("admin-language") || 'de',
-              'datepicker' : false,
-              'format' : 'H:i',
-              'onChangeDateTime' : _me._onChangeDateTime
+        _me._htmlElement.find('input.cel_timePicker').each(function (key, element) {
+          let pickerAttrObj = {
+            // FIXME [CELDEV-904] DateTimePicker Language timing issue
+            'lang': Validation.messages.get("admin-language") || 'de',
+            'datepicker': false,
+            'format': 'H:i',
+            'onChangeDateTime': _me._onChangeDateTime
           };
-          var pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
-          if(pickerDataAttrObj) {
+          const pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
+          if (pickerDataAttrObj) {
             pickerAttrObj = $j.extend(pickerAttrObj, pickerDataAttrObj);
           }
           $j(element).datetimepicker(pickerAttrObj);
@@ -100,23 +93,23 @@
          * Sample with additional Attribute cel_dateTimePicker:
          * <input type="text" class="cel_dateTimePicker" data-pickerAttr='{"format":"d/m/Y H:i"}'>
          */
-        _me._htmlElement.find('input.cel_dateTimePicker').each(function(key, element){
-          var pickerAttrObj = {
-              // FIXME [CELDEV-904] DateTimePicker Language timing issue
-              'lang' : Validation.messages.get("admin-language") || 'de',
-              'dayOfWeekStart' : 1,
-              'minDate' : 0,
-              'format' : 'd.m.Y H:i',
-              'onChangeDateTime' : _me._onChangeDateTime
+        _me._htmlElement.find('input.cel_dateTimePicker').each(function (key, element) {
+          let pickerAttrObj = {
+            // FIXME [CELDEV-904] DateTimePicker Language timing issue
+            'lang': Validation.messages.get("admin-language") || 'de',
+            'dayOfWeekStart': 1,
+            'minDate': 0,
+            'format': 'd.m.Y H:i',
+            'onChangeDateTime': _me._onChangeDateTime
           };
-          var pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
-          if(pickerDataAttrObj) {
+          const pickerDataAttrObj = JSON.parse(element.getAttribute('data-pickerAttr'));
+          if (pickerDataAttrObj) {
             pickerAttrObj = $j.extend(pickerAttrObj, pickerDataAttrObj);
           }
           $j(element).datetimepicker(pickerAttrObj);
           $(element).observe('change', _me._onChangeEventBind);
         });
       }
-  };
-
+    });
+  }
 })(window)
