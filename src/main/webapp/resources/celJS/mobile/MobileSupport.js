7367fc883b01299461116d8697ae77f124f39a5d
Fabian <fabian.pichler@synventis.com>
2021-03-31 16:56:13 +0200

    CELDEV-952 - drop IE < 9 support and correct transform-scale (#255)
    * refactor and correct getZoomStyle for css3
    
    * replace 'var'
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * activate debugging
    
    * change to scrollHeight and scrollWidth
    
    * debuging
    
    * debuging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * set slideRoot postion to absolute before reading scrollWidth
    
    * prepare PR
    
    * syntax and formatting
    
    * Update src/main/webapp/resources/celJS/mobile/MobileSupport.js
    
    Co-authored-by: Marc Sladek <marc@sladek.me>
    
    * refine logging levels
    
    Co-authored-by: Marc Sladek <marc@sladek.me>

--- src/main/webapp/resources/celJS/mobile/MobileSupport.js
+++ src/main/webapp/resources/celJS/mobile/MobileSupport.js
@@ -22,91 +22,91 @@
  * Celements mobile support library
  * This is the Celements mobile support library.
  */
-if(typeof CELEMENTS=="undefined"){var CELEMENTS={};};
-if(typeof CELEMENTS.mobile=="undefined"){CELEMENTS.mobile={};};
+(function (window, undefined) {
 
-(function(window, undefined) {
+  if (typeof window.CELEMENTS === "undefined") { window.CELEMENTS = {}; }
+  if (typeof window.CELEMENTS.mobile === "undefined") { window.CELEMENTS.mobile = {}; }
 
-//////////////////////////////////////////////////////////////////////////////
-//Celements mobile dimensions
-//////////////////////////////////////////////////////////////////////////////
-CELEMENTS.mobile.Dimensions = function() {
-  // constructor
-  this._init();
-};
+  //////////////////////////////////////////////////////////////////////////////
+  //Celements mobile dimensions
+  //////////////////////////////////////////////////////////////////////////////
+  CELEMENTS.mobile.Dimensions = function () {
+    // constructor
+    this._init();
+  };
 
-CELEMENTS.mobile.Dimensions.prototype = {
-    
-    _dimLogging : false,
+  CELEMENTS.mobile.Dimensions.prototype = {
 
-    isMobile : {
-      Safari: function() {
-        var _me = this;
+    _dimLogging: false,
+
+    isMobile: {
+      Safari: function () {
+        const _me = this;
         return window.navigator.userAgent.match(/Safari/i) && !_me.Chrome()
-                && !_me.Windows();
+          && !_me.Windows();
       },
-      Chrome: function() {
+      Chrome: function () {
         return window.navigator.userAgent.match(/Chrome|CriOS/i);
       },
-      Android: function() {
+      Android: function () {
         return window.navigator.userAgent.match(/Android/i);
       },
-      BlackBerry: function() {
+      BlackBerry: function () {
         return window.navigator.userAgent.match(/BlackBerry/i);
       },
-      iOS: function() {
+      iOS: function () {
         return window.navigator.userAgent.match(/iPhone|iPad|iPod/i);
       },
-      iPhone: function() {
+      iPhone: function () {
         return window.navigator.userAgent.match(/iPhone/i);
       },
-      iPod: function() {
+      iPod: function () {
         return window.navigator.userAgent.match(/iPod/i);
       },
-      iPad: function() {
+      iPad: function () {
         return window.navigator.userAgent.match(/iPad/i);
       },
-      Opera: function() {
+      Opera: function () {
         return window.navigator.userAgent.match(/Opera Mini/i);
       },
-      Windows: function() {
+      Windows: function () {
         return window.navigator.userAgent.match(/IEMobile/i);
       },
-      IE: function() {
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-        var trident = ua.match(/Trident.*rv\:(\d{2})\./) || false;
-        var edge = ua.match(/Edge\/(\d{2})\./) || false;
+      IE: function () {
+        const ua = window.navigator.userAgent;
+        const msie = ua.indexOf("MSIE ");
+        const trident = ua.match(/Trident.*rv\:(\d{2})\./) || false;
+        const edge = ua.match(/Edge\/(\d{2})\./) || false;
         return (msie > 0 || trident || edge);
       },
-      Simulator: function() {
+      Simulator: function () {
         // http://iphone4simulator.com/ maybe
         return (window.top != window);
       },
-      ChromeOn_iOS: function() {
+      ChromeOn_iOS: function () {
         return window.navigator.userAgent.match(/CriOS/i);
       },
-      any: function() {
-        var _me = this;
+      any: function () {
+        const _me = this;
         return (_me.Android() || _me.BlackBerry() || _me.iOS() || _me.Opera()
-            || _me.Windows());
+          || _me.Windows());
       }
     },
 
-    _init : function() {
+    _init: function () {
     },
 
-    version: function() {
-      var _me = this;
-      var ua = window.navigator.userAgent;
+    version: function () {
+      const _me = this;
+      const ua = window.navigator.userAgent;
       if (_me.isMobile.Safari()) {
         return ua.match(/Version\/(\d+\.\d+)/)[1];
       } else if (_me.isMobile.Chrome()) {
         return ua.match(/Chrome|CriOS\/(\d+\.\d+)/)[1];
-      } else  if (_me.isMobile.IE()) {
-        var msie = ua.indexOf("MSIE ");
-        var trident = ua.match(/Trident.*rv\:(\d{2})\./) || false;
-        var edge = ua.match(/Edge\/(\d{2})\./) || false;
+      } else if (_me.isMobile.IE()) {
+        const msie = ua.indexOf("MSIE ");
+        const trident = ua.match(/Trident.*rv\:(\d{2})\./) || false;
+        const edge = ua.match(/Edge\/(\d{2})\./) || false;
         if (trident) {
           return parseInt(trident[1]);
         } else if (edge) {
@@ -118,13 +118,10 @@ CELEMENTS.mobile.Dimensions.prototype = {
       return $j.browser.version;
     },
 
-    getZoomStyles : function(zoomFactor, fullWidth, fullHeight) {
-      var _me = this;
-      var zoomStyles = {};
-      //zoom for IE < 9 else transform:scale
-      if (_me.isMobile.IE() && (_me.version() < 9)) {
-        zoomStyles['zoom'] = zoomFactor;
-      } else if (_me.isMobile.IE() && (_me.version() < 10)) {
+    getZoomStyles: function (zoomFactor, fullWidth, fullHeight) {
+      const _me = this;
+      const zoomStyles = {};
+      if (_me.isMobile.IE() && (_me.version() < 10)) {
         zoomStyles['msTransformOrigin'] = '0 0'; //IE9 only supports 2D values
         zoomStyles['msTransform'] = 'scale(' + zoomFactor + ')';
         zoomStyles['transformOrigin'] = '0 0 0';
@@ -135,25 +132,36 @@ CELEMENTS.mobile.Dimensions.prototype = {
         zoomStyles['transformOrigin'] = '0 0 0';
         zoomStyles['transform'] = 'scale(' + zoomFactor + ')';
       }
-      zoomStyles['height'] = fullHeight + 'px';  // important for FF
-      zoomStyles['width'] = fullWidth + 'px';  // important for FF
+      if (!Number.isNaN(fullHeight)) {
+        // transform-scale only scales the content of the box, the box must be resized itself
+        const scaledHeight = (zoomFactor * fullHeight);
+        zoomStyles['height'] = scaledHeight + 'px';
+      } else {
+        zoomStyles['height'] = fullHeight;
+      }
+      if (!Number.isNaN(fullHeight)) {
+        // transform-scale only scales the content of the box, the box must be resized itself
+        const scaledWidth = (zoomFactor * fullWidth);
+        zoomStyles['width'] = scaledWidth + 'px';
+      } else {
+        zoomStyles['width'] = fullWidth;
+      }
       return zoomStyles;
     },
 
-    isOrientationLandscape : function() {
-//    var _me = this;
-      var innerWidth = window.innerWidth || document.documentElement.clientWidth;
-      var innerHeight = window.innerHeight || document.documentElement.clientHeight;
+    isOrientationLandscape: function () {
+      const innerWidth = window.innerWidth || document.documentElement.clientWidth;
+      const innerHeight = window.innerHeight || document.documentElement.clientHeight;
       //window.orientation works only correct on load, but has whimsical behavior when 
       //  rotating 
       return innerWidth > innerHeight;
     },
-  
-    getInnerWidth : function() {
-      var _me = this;
-      var width = window.innerWidth || document.documentElement.clientWidth;
-      if(_me.isMobile.any()) {
-        if(_me.isMobile.iOS() && _me.isOrientationLandscape()) {
+
+    getInnerWidth: function () {
+      const _me = this;
+      let width = window.innerWidth || document.documentElement.clientWidth;
+      if (_me.isMobile.any()) {
+        if (_me.isMobile.iOS() && _me.isOrientationLandscape()) {
           width = screen.height;
         } else if (!_me.isMobile.Android()) {
           width = screen.width;
@@ -165,11 +173,11 @@ CELEMENTS.mobile.Dimensions.prototype = {
       return width;
     },
 
-    getInnerHeight : function() {
-      var _me = this;
-      var height = window.innerHeight || document.documentElement.clientHeight;
-      if(_me.isMobile.any()) {
-        if(_me.isMobile.iOS() && _me.isOrientationLandscape()) {
+    getInnerHeight: function () {
+      const _me = this;
+      let height = window.innerHeight || document.documentElement.clientHeight;
+      if (_me.isMobile.any()) {
+        if (_me.isMobile.iOS() && _me.isOrientationLandscape()) {
           height = screen.width;
         } else if (!_me.isMobile.Android()) {
           height = screen.height;
@@ -181,44 +189,44 @@ CELEMENTS.mobile.Dimensions.prototype = {
       return height;
     },
 
-    getDimensions : function() {
-      var mobileDim = {
-          'screen' : {
-            'width' : screen.width,
-            'height' :screen.height
-          },
-          'screenAvailable' : {
-            'width' : screen.availWidth,
-            'height' : screen.availHeight
-          },
-          'windowInner' : {
-            'width' : window.innerWidth,
-            'height' : window.innerHeight
-          },
-          'windowOuter' : {
-            'width' : window.outerWidth,
-            'height' : window.outerHeight
-          },
-          'docBodyClient' : {
-            'width' : document.body.clientWidth,
-            'height' : document.body.clientHeight
-          },
-          'docBodyOffset' : {
-            'width' : document.body.offsetWidth,
-            'height' : document.body.offsetHeight
-          },
-          'docElement' : {
-            'width' : document.documentElement.clientWidth,
-            'height' : document.documentElement.clientHeight
-          }
+    getDimensions: function () {
+      const mobileDim = {
+        'screen': {
+          'width': screen.width,
+          'height': screen.height
+        },
+        'screenAvailable': {
+          'width': screen.availWidth,
+          'height': screen.availHeight
+        },
+        'windowInner': {
+          'width': window.innerWidth,
+          'height': window.innerHeight
+        },
+        'windowOuter': {
+          'width': window.outerWidth,
+          'height': window.outerHeight
+        },
+        'docBodyClient': {
+          'width': document.body.clientWidth,
+          'height': document.body.clientHeight
+        },
+        'docBodyOffset': {
+          'width': document.body.offsetWidth,
+          'height': document.body.offsetHeight
+        },
+        'docElement': {
+          'width': document.documentElement.clientWidth,
+          'height': document.documentElement.clientHeight
+        }
       };
       return mobileDim;
     },
 
-    alertDim : function() {
-      var _me = this;
-      var mobileDim = _me.getDimensions();
-      var outStr = "dimensions: \n";
+    alertDim: function () {
+      const _me = this;
+      const mobileDim = _me.getDimensions();
+      let outStr = "dimensions: \n";
       outStr += "screen: " + mobileDim.screen.width + ","
         + mobileDim.screen.height + "\n";
       outStr += "screenAvailable: " + mobileDim.screenAvailable.width + ","
@@ -236,32 +244,29 @@ CELEMENTS.mobile.Dimensions.prototype = {
       alert(outStr);
     },
 
-    logDimAndAgent : function(message) {
-      var _me = this;
-      var logMessage = message || '';
+    logDimAndAgent: function (message) {
+      const _me = this;
+      const logMessage = message || '';
       new Ajax.Request(getCelHost(), {
-        method : "POST",
+        method: "POST",
         parameters: {
-          'xpage' : 'celements_ajax',
-          'ajax_mode' : 'MobileLogDimAndAgent',
-          'mobileDim' : Object.toJSON(_me.getDimensions()),
-          'userAgent' : window.navigator.userAgent,
-          'isOrientationLandscape' : _me.isOrientationLandscape(),
-          'message' : logMessage
+          'xpage': 'celements_ajax',
+          'ajax_mode': 'MobileLogDimAndAgent',
+          'mobileDim': Object.toJSON(_me.getDimensions()),
+          'userAgent': window.navigator.userAgent,
+          'isOrientationLandscape': _me.isOrientationLandscape(),
+          'message': logMessage
         },
-        onSuccess : function(transport) {
+        onSuccess: function (transport) {
           if (transport.responseText.isJSON()) {
-            var responseObject = transport.responseText.evalJSON();
-            if ((typeof console != 'undefined') && (typeof console.log != 'undefined')) {
-              console.log('MobileLogDimAndAgent finished successfully. ', responseObject);
-            }
-          } else if ((typeof console != 'undefined')
-              && (typeof console.error != 'undefined')) {
+            const responseObject = transport.responseText.evalJSON();
+            console.log('MobileLogDimAndAgent finished successfully. ', responseObject);
+          } else {
             console.error('noJSON!!! ', transport.responseText);
           }
         }
       });
     }
-};
+  };
 
 })(window);
