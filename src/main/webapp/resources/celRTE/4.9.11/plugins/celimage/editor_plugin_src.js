7b63fff10a89105433c13204bf2817db3dfebbda
Fabian Pichler <fabian.pichler@synventis.com>
2022-05-18 07:01:21 +0200

    update versioned url

--- src/main/webapp/resources/celRTE/4.9.11/plugins/celimage/editor_plugin_src.js
+++ src/main/webapp/resources/celRTE/4.9.11/plugins/celimage/editor_plugin_src.js
@@ -17,7 +17,7 @@
           return;
 
         ed.windowManager.open({
-          file : url + '/image.htm',
+          file : url + '/image.htm?version=202205180630',
           width : 743 + parseInt(ed.getLang('celimage.delta_width', 0)),
           height : 670 + parseInt(ed.getLang('celimage.delta_height', 0)),
           inline : 1
