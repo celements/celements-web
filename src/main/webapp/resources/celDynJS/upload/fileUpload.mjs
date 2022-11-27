434a6b865a501bec6ba2ffe26d701429cd215db4
Fabian Pichler <fabian.pichler@synventis.com>
2022-11-27 13:45:32 +0100

    add dropOver styles

---
+++ src/main/webapp/resources/celDynJS/upload/fileUpload.mjs
@@ -0,0 +1,120 @@
+
+export class CelUploadHandler {
+  
+  constructor(uploadUrl, fileBaseUrl) {
+    this.#uploadUrl = uploadUrl;
+    this.#fileBaseUrl = fileBaseUrl;
+  }
+
+  #createXhrForUpload(resolve, reject, progress) {
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.open('POST', this.#uploadUrl + '?xpage=celements_ajax&ajax_mode=TokenFileUploader&tfu_mode=upload');
+  
+    xhr.upload.onprogress = (e) => {
+      progress(Math.round(e.loaded / e.total * 100));
+    };
+  
+    xhr.onload = () => {
+      if ((xhr.status >= 200) && (xhr.status < 300)) {
+        try {
+          const json = JSON.parse(xhr.responseText);
+          if (!json || typeof json.success != 'boolean') {
+            reject('Invalid JSON: ' + xhr.responseText);
+          }
+          if (!json.success) {
+            reject('failed to upload: ' + xhr.responseText);
+          }
+          resolve(this.#fileBaseUrl + '/'+ json.attfilename);
+        } catch (err) {
+          reject('Invalid JSON: ' + xhr.responseText + ' parse error: ' + err);
+        }
+      } else if (xhr.status === 403) {
+        reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
+      } else {
+        reject('HTTP Error: ' + xhr.status);
+      }
+    };
+  
+    xhr.onerror = () => {
+      reject('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
+    };
+    return xhr;
+  }
+
+  upload(fileInfo, progress) {
+    return  new Promise((resolve, reject) => {        
+      fetch('?xpage=celements_ajax&ajax_mode=TokenFileUploader&tfu_mode=getTokenForCurrentUser')
+      .then((response) => response.json())
+      .then((respJson) => {
+        console.log('celRTE_image_upload_handler respJson', respJson, respJson.token);
+        const xhr = this.#createXhrForUpload(resolve, reject, progress);
+        console.log('celRTE_image_upload_handler token[', respJson.token, '] filename [',
+          fileInfo.name, ']');
+        const formData = new FormData();
+        formData.append('uploadToken', respJson.token);
+        formData.append('celTokenUploadCreateIfNotExists', true);
+        formData.append('filename', fileInfo.name);
+        formData.append('filepath', fileInfo.blob, fileInfo.name);
+        xhr.send(formData);
+      }).catch((err) => reject({ message: 'Failed to get upload token. ' + err, remove: true  }));
+    });
+  }
+}
+
+export class CelFileDropHandler {
+  
+  constructor(uploadUrl, fileBaseUrl) {
+    this.#uploadHandler = new CelUploadHandler(uploadUrl, fileBaseUrl);
+  }
+
+  registerHandler(dropZoneElem) {
+    this.dropZoneElem = dropZoneElem;
+    this.dropZoneElem.addEventListener('drop', (event) => this.dropHandler(event));
+    this.dropZoneElem.addEventListener('dragover', (event) => this.dragOverHandler(event));
+    this.dropZoneElem.addEventListener('dragend', (event) => this.dragEndHandler(event));
+    this.dropZoneElem.addEventListener('dragleave', (event) => this.dragEndHandler(event));
+  }
+
+  dragEndHandler(ev) {
+    console.log('dragEnd ', ev);
+    this.dropZoneElem.classList.remove('celDropOverActive');
+  }
+
+  dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+    this.dropZoneElem.classList.add('celDropOverActive');
+  }
+
+  dropHandler(ev) {
+    console.log('File(s) dropped');
+    ev.preventDefault();
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === 'file') {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+          this.#uploadHandler.upload({
+              'name' : file.name,
+              'blob' : file
+            }, (standPercent) => console.log('upload1 progress ', standPercent)
+          ).then(() => this.updateAttachmentList());
+        }
+      });
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+          return this.#uploadHandler.upload({
+            'name' : file.name,
+            'blob' : file
+          }, (standPercent) => console.log('upload2 progress ', standPercent));
+      });
+    }
+  }
+  
+}
--- src/main/webapp/resources/celRTE/plugins-v6/bellis-test.mjs
+++ src/main/webapp/resources/celRTE/plugins-v6/bellis-test.mjs
@@ -1,115 +1,5 @@
 
-class CelUploadHandler {
-  
-  constructor(uploadUrl, fileBaseUrl) {
-    this.uploadUrl = uploadUrl;
-    this.fileBaseUrl = fileBaseUrl;
-  }
-
-  #createXhrForUpload(resolve, reject, progress) {
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('POST', this.uploadUrl + '?xpage=celements_ajax&ajax_mode=TokenFileUploader&tfu_mode=upload');
-  
-    xhr.upload.onprogress = (e) => {
-      progress(Math.round(e.loaded / e.total * 100));
-    };
-  
-    xhr.onload = () => {
-      if ((xhr.status >= 200) && (xhr.status < 300)) {
-        try {
-          const json = JSON.parse(xhr.responseText);
-          if (!json || typeof json.success != 'boolean') {
-            reject('Invalid JSON: ' + xhr.responseText);
-          }
-          if (!json.success) {
-            reject('failed to upload: ' + xhr.responseText);
-          }
-          resolve(this.fileBaseUrl + '/'+ json.attfilename);
-        } catch (err) {
-          reject('Invalid JSON: ' + xhr.responseText + ' parse error: ' + err);
-        }
-      } else if (xhr.status === 403) {
-        reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
-      } else {
-        reject('HTTP Error: ' + xhr.status);
-      }
-    };
-  
-    xhr.onerror = () => {
-      reject('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
-    };
-    return xhr;
-  }
-
-  upload(fileInfo, progress) {
-    return  new Promise((resolve, reject) => {        
-      fetch('?xpage=celements_ajax&ajax_mode=TokenFileUploader&tfu_mode=getTokenForCurrentUser')
-      .then((response) => response.json())
-      .then((respJson) => {
-        console.log('celRTE_image_upload_handler respJson', respJson, respJson.token);
-        const xhr = this.#createXhrForUpload(resolve, reject, progress);
-        console.log('celRTE_image_upload_handler token[', respJson.token, '] filename [',
-          fileInfo.name, ']');
-        const formData = new FormData();
-        formData.append('uploadToken', respJson.token);
-        formData.append('celTokenUploadCreateIfNotExists', true);
-        formData.append('filename', fileInfo.name);
-        formData.append('filepath', fileInfo.blob, fileInfo.name);
-        xhr.send(formData);
-      }).catch((err) => reject({ message: 'Failed to get upload token. ' + err, remove: true  }));
-    });
-  }
-}
-
-class CelFileDropHandler {
-  
-  constructor(uploadUrl, fileBaseUrl) {
-    this.uploadHandler = new CelUploadHandler(uploadUrl, fileBaseUrl);
-  }
-
-  registerHandler(dropZoneElem) {
-    this.dropZoneElem = dropZoneElem;
-    this.dropZoneElem.addEventListener('drop', (event) => this.dropHandler(event));
-    this.dropZoneElem.addEventListener('dragover', (event) => this.dragOverHandler(event));
-  }
-
-  dragOverHandler(ev) {
-    console.log('File(s) in drop zone');
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  }
-
-  dropHandler(ev) {
-    console.log('File(s) dropped');
-    ev.preventDefault();
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        // If dropped items aren't files, reject them
-        if (item.kind === 'file') {
-          const file = item.getAsFile();
-          console.log(`… file[${i}].name = ${file.name}`);
-          this.uploadHandler.upload({
-              'name' : file.name,
-              'blob' : file
-            }, (standPercent) => console.log('upload1 progress ', standPercent)
-          ).then(() => this.updateAttachmentList());
-        }
-      });
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        console.log(`… file[${i}].name = ${file.name}`);
-          return this.uploadHandler.upload({
-            'name' : file.name,
-            'blob' : file
-          }, (standPercent) => console.log('upload2 progress ', standPercent));
-      });
-    }
-  }
-  
-}
+import { CelUploadHandler, CelFileDropHandler } from "/file/resources/celDynJs/upload/fileUpload.mjs?version=202211271330";
 
 class CelFilePicker {
 
@@ -211,12 +101,12 @@ class CelRteAdaptor {
   constructor(options) {
     this.options = options
     this.filePicker = new CelFilePicker(options);
-    this.uploadHandler = new CelUploadHandler(options.wiki_attach_path,
+    this.#uploadHandler = new CelUploadHandler(options.wiki_attach_path,
       options.wiki_imagedownload_path);
   }
 
   uploadImagesHandler(blobInfo, progress) {
-    return this.uploadHandler.upload({
+    return this.#uploadHandler.upload({
       'name' : blobInfo.filename(),
       'blob' : blobInfo.blob()
     }, progress);
