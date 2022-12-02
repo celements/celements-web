eb66ddccacb190b201570f459ccd453b12c93f08
Fabian Pichler <fabian.pichler@synventis.com>
2022-12-02 07:45:32 +0100

    merge CELDEV-1072

--- src/main/webapp/resources/celRTE/plugins-v6/bellis-test.mjs
+++ src/main/webapp/resources/celRTE/plugins-v6/bellis-test.mjs
@@ -1,8 +1,8 @@
 
 import { CelUploadHandler, CelFileDropHandler }
-  from "/file/resources/celDynJS/upload/fileUpload.mjs?version=202211020741";
+  from "/file/resources/celDynJS/upload/fileUpload.mjs?version=202211020745";
 import { CelOverlay }
-  from "/file/resources/celDynJS/overlay/celOverlay.mjs?version=202211020741";
+  from "/file/resources/celDynJS/overlay/celOverlay.mjs?version=202211020745";
 
 class CelFilePicker {
 
