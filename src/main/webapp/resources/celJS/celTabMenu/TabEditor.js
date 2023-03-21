bd28807f903305699ec9d90fcd5fa2dfae69c4fc
Fabian Pichler <fabian.pichler@synventis.com>
2023-03-21 15:55:26 +0100

    remove debugging alert

--- src/main/webapp/resources/celJS/celTabMenu/TabEditor.js
+++ src/main/webapp/resources/celJS/celTabMenu/TabEditor.js
@@ -453,7 +453,6 @@ TE.prototype = {
         } catch (exp) {
           console.error('Saved-listener failed.', exp);
         }
-        alert('debugging?');
         if (!failed) {
           window.onbeforeunload = null;
           window.location.href = _me._getCancelURL();
