ea5d2a6ec03bfa0d4d57a7795183a7c460ff691e
Fabian Pichler <fabian.pichler@synventis.com>
2023-04-12 22:24:02 +0200

    add comment

--- src/main/webapp/resources/celJS/prototype.js
+++ src/main/webapp/resources/celJS/prototype.js
@@ -1,5 +1,5 @@
 /*  Prototype JavaScript framework, version 1.7.2
- *  (c) 2005-2010 Sam Stephenson
+ *  (c) 2005-2010 Sam Stephenson, (c) 2023 forking to transition to vanilla js
  *
  *  Prototype is freely distributable under the terms of an MIT-style license.
  *  For details, see the Prototype web site: http://www.prototypejs.org/
