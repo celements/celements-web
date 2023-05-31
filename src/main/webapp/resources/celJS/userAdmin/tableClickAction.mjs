646c3e7b8dbb8f5228d9222a369e52c8e1dd4bd1
Chantal Pichler <chantal.pichler@synventis.com>
2023-05-31 11:33:47 +0200

    changing EventListeners for tableAction

--- src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
+++ src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
@@ -1,7 +1,17 @@
-document.querySelectorAll('.struct_table_data .struct_table_row').forEach(entry => { 
+document.querySelectorAll('ul.struct_table_data > li.struct_table_row')
+            .forEach(entry => registerListeners(entry));
+
+function registerListeners(entry){
+  entry.addEventListener("mouseover", () => {entry.classList.add('open')});
+  entry.addEventListener("mouseout", () => {entry.classList.remove('open')});
+}
+
+
+
+/*document.querySelectorAll('.struct_table_data .struct_table_row').forEach(entry => { 
   entry.addEventListener("click", () => {
      document.querySelectorAll('.struct_table_data .struct_table_row').forEach(e => 
               e.classList.remove('open'));
           entry.classList.add('open');
   }); 
-});
+});*/
