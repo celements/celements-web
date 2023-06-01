f8bedb0e9085512538d8fa5d0701eaa0082b0909
Chantal Pichler <chantal.pichler@synventis.com>
2023-06-01 11:58:01 +0200

    refactoring tableClickAction and add changing icons

--- src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
+++ src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
@@ -1,10 +1,14 @@
 document.querySelectorAll('ul.struct_table_data li.struct_table_row .column_arrow')
-            .forEach(arrow => registerListeners(arrow));
+            .forEach(arrow => arrow.addEventListener("click", openCloseTableRow(arrow)));
             
-function registerListeners(arrow){
+function openCloseTableRow(arrow){
   const entry = arrow.closest('ul.struct_table_data li.struct_table_row');
-  console.log(entry)
-  arrow.addEventListener("click", () => {entry.classList.toggle('open')});
+  entry.classList.toggle('open');
+  if(classList.contains('open')){
+    arrow.innerHTML = <i class="fa fa-caret-down"></i>
+  } else {
+    arrow.innerHTML = <i class="fa fa-caret-right"></i>
+  }
 }
 
 /* Tipp von Marc: click Event registrieren auf Body. bei jedem Klick den DOMTree hochgehen und prüfen, 
