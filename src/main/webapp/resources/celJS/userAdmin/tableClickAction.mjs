b69a56f10cb76e33dfbb5d3aa8fcd5429ed7ef26
Chantal Pichler <chantal.pichler@synventis.com>
2023-06-02 17:04:49 +0200

    correct openCloseTableRow again

--- src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
+++ src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
@@ -1,9 +1,13 @@
 document.querySelectorAll('ul.struct_table_data li.struct_table_row .column_arrow')
-            .forEach(arrow => arrow.addEventListener("click", openCloseTableRow(arrow)));
-            
-function openCloseTableRow(arrow){
-  const entry = arrow.closest('ul.struct_table_data li.struct_table_row');
+     .forEach(arrow => arrow.addEventListener("click", () => openCloseTableRow(arrow)));
+     
+function openCloseTableRow(arrow) {
   entry.classList.toggle('open');
+  if (entry.classList.contains('open')) {
+    arrow.innerHTML = '<i class="fa fa-caret-down"></i>';
+  } else {
+    arrow.innerHTML = '<i class="fa fa-caret-right"></i>';
+  }
 }
 
 /* Tipp von Marc: click Event registrieren auf Body. bei jedem Klick den DOMTree hochgehen und prüfen, 
