fc63de34715918389b4b4e0f55716d4601eadfd3
Chantal Pichler <chantal.pichler@synventis.com>
2023-06-07 11:05:26 +0200

    change showUserActive to add icon to classlist

--- src/main/webapp/resources/celJS/userAdmin/showUserActive.mjs
+++ src/main/webapp/resources/celJS/userAdmin/showUserActive.mjs
@@ -1,13 +1,13 @@
-document.querySelectorAll('ul.struct_table_data li.struct_table_row .column_active')
-     .forEach(user => checkIfUserActive(user));
+document.querySelectorAll('ul.struct_table_data li.struct_table_row .column_active i.fa')
+     .forEach(icon => checkIfUserActive(icon));
 
 function checkIfUserActive(user){
     var randomBoolean = Math.random() >= 0.5;
     if(randomBoolean){
-    //if user is active and not suspended add class active and show icon "fa fa-user"
-    user.classList.add('active');
+    //if user is active and not suspended show icon "fa fa-user"
+    icon.classList.add('fa-user');
     } else {
-    //if user is inactive or suspended add class inactive and show icon "fa fa-ban"
-    user.classList.add('inactive');
+    //if user is inactive or suspended show icon "fa fa-ban"
+    icon.classList.add('fa-ban');
     }
 }
