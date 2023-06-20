3e089c9ade33529d724c08cb52e19ec521b32c32
Marc Sladek <marc@sladek.dev>
2023-06-20 19:09:37 +0200

    update

--- docker/database/init/01-create-main.sh
+++ docker/database/init/01-create-main.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "** Creating ${DB_PREFIX}_main"
+DB_NAME="${DB_PREFIX}_${DB_MAIN}"
+echo "** Creating ${DB_NAME}"
 mysql -u root -p$MYSQL_ROOT_PASSWORD --execute "
-CREATE DATABASE IF NOT EXISTS ${DB_PREFIX}_main;
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 GRANT all privileges ON \`${DB_PREFIX}\\_%\`.* TO '${DB_USER}'@'%' identified BY '${DB_PASSWORD}';"
 echo "** done"
--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
 echo "** installing hibernate.cfg.xml"
 envsubst < /usr/local/config/hibernate.cfg.xml \
   > ${CATALINA_HOME}/webapps/ROOT/WEB-INF/hibernate.cfg.xml
 
-# exec CMD
+# TODO xwiki.cfg
+
+echo "** installing log4j2.properties"
+envsubst < /usr/local/config/log4j2.properties \
+  > ${CATALINA_HOME}/webapps/ROOT/WEB-INF/classes/log4j2.properties
+
 exec "$@"
