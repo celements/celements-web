e31bca0734c8eadfef8a4ab339900fdc5f6c4dbe
Chantal Pichler <chantal.pichler@synventis.com>
2023-08-02 09:10:03 +0200

    delete file and use JSEventManager instead

--- src/main/webapp/resources/celJS/userAdmin/tableClickAction.mjs
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('ul.struct_table_data li.struct_table_row .column_arrow')
-     .forEach(arrow => arrow.addEventListener("click", () => openCloseTableRow(arrow)));
-     
-function openCloseTableRow(arrow) {
-  const entry = arrow.closest('ul.struct_table_data li.struct_table_row');
-  entry.classList.toggle('open');
-}
