10d013fd943b95961bf75f98039fac8b39b552b9
Marc Sladek <marc@sladek.dev>
2023-09-18 02:17:27 +0200

    update version

--- src/main/webapp/resources/celDynJS/celData/cel-data-renderer.mjs
+++ src/main/webapp/resources/celDynJS/celData/cel-data-renderer.mjs
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-import './cel-data.mjs?version=20230806';
+import './cel-data.mjs?version=20230917';
 
 export default class CelDataRenderer {
 
