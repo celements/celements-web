e71a66fc3f1dc69536d4b2928cacdbf0e4ae5cdd
Fabian Pichler <fabian.pichler@synventis.com>
2023-10-07 00:49:32 +0200

    add await

--- src/main/webapp/resources/celDynJS/celData/cel-data.mjs
+++ src/main/webapp/resources/celDynJS/celData/cel-data.mjs
@@ -191,7 +191,7 @@ export class CelDataImage extends CelData {
   }
 
   async urlImageSrc(data, extractMode) {
-    const src = this.extractValue(data, extractMode);
+    const src = await this.extractValue(data, extractMode);
     if (src) {
       const url = new URL(src);
       for (const [key, value] of new URLSearchParams(this.imgSrcParams)) {
