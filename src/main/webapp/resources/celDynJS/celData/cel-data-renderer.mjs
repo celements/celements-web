1a7957ea2b7dd7f519ab10e16ee948049b1c5124
Fabian Pichler <fabian.pichler@synventis.com>
2023-10-07 00:04:40 +0200

    versioning

--- src/main/webapp/resources/celDynJS/celData/cel-data-renderer.mjs
+++ src/main/webapp/resources/celDynJS/celData/cel-data-renderer.mjs
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-import './cel-data.mjs?version=202310062355';
+import './cel-data.mjs?version=20231007';
 
 export default class CelDataRenderer {
 
