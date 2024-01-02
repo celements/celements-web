8d27b0470143d8425099d620be947ead329173ce
Fabian Pichler <fabian.pichler@synventis.com>
2024-01-02 23:18:40 +0100

    PROZHP-142 - Ersatz für Google Analytics auf ProZ (#427)
    * update analyticsQueue.js to work with Matomo backend
    
    * add lost looping of send
    
    * finalize change to matomo
    
    * mark analyticsQueue.js as deprecated
    
    * refactorings
    
    * refactor analyticsQueue.mjs
    
    * debugging
    
    * replace _mtm with _paq
    
    * add eventCategory and eventVlaue
    
    * Update src/main/webapp/resources/celRes/analyticsQueue.js
    
    Co-authored-by: Edo Beutler <edoardo.beutler@synventis.com>
    
    * Update src/main/webapp/resources/celDynJS/analytics/analyticsQueue.mjs
    
    Co-authored-by: Edo Beutler <edoardo.beutler@synventis.com>
    
    * Update src/main/webapp/resources/celDynJS/analytics/analyticsQueue.mjs
    
    Co-authored-by: Marc Sladek <marc@sladek.me>
    
    * Update src/main/webapp/resources/celDynJS/analytics/analyticsQueue.mjs
    
    Co-authored-by: Marc Sladek <marc@sladek.me>
    
    * PR comments
    
    ---------
    
    Co-authored-by: Edo Beutler <edoardo.beutler@synventis.com>
    Co-authored-by: Marc Sladek <marc@sladek.me>

--- src/main/webapp/resources/celRes/analyticsQueue.js
+++ src/main/webapp/resources/celRes/analyticsQueue.js
@@ -21,12 +21,11 @@
 (function(window, undefined) {
   "use strict";
 
+  console.warn('analyticsQueue.js is deprecated since November 2023.'
+    + ' Instead use celDynJS/analytics/analyticsQueue.mjs');
+
   var Queue = Class.create({
-    maxHitsPerSession : 500,
     _running : false,
-    _availableHits : 20,
-    _analytics : undefined,
-    _analyticsId : undefined,
     _analyticsEventQueue : undefined,
     _analyticsPriorizedEventQueue : undefined,
     _addBind : undefined,
@@ -34,48 +33,32 @@
     _sizeBind : undefined,
     _lowPrioritySizeBind : undefined,
     _highPrioritySizeBind : undefined,
-    _sentHitsCountBind : undefined,
-    _sendSessionOutOfHitsBind : undefined,
-    _atHitsPerSessionLimitBind : undefined,
     _sendHitsBind : undefined,
     _getPrioMsgPartBind : undefined,
 
     initialize : function() {
-      var _me = this;
-      if (window.ga) {
-        _me._analytics = window.ga;
-        _me._analyticsEventQueue = new Array();
-        _me._analyticsPriorizedEventQueue = new Array();
-        _me._addBind = _me.add.bind(_me);
-        _me._priorityAddBind = _me.priorityAdd.bind(_me);
-        _me._sizeBind = _me.size.bind(_me);
-        _me._lowPrioritySizeBind = _me.lowPrioritySize.bind(_me);
-        _me._highPrioritySizeBind = _me.highPrioritySize.bind(_me);
-        _me._sentHitsCountBind = _me.sentHitsCount.bind(_me);
-        _me._sendSessionOutOfHitsBind = _me._sendSessionOutOfHits.bind(_me);
-        _me._atHitsPerSessionLimitBind = _me._atHitsPerSessionLimit.bind(_me);
-        _me._sendHitsBind = _me._sendHits.bind(_me);
-        _me._getPrioMsgPartBind = _me._getPrioMsgPart.bind(_me);
-        var metas = $$('meta[name="cel-GAA-Num"]');
-        if((metas.size() > 0) && (metas[0].content !== '')) {
-          _me._analyticsId = metas[0].content;
-          _me._running = true;
-          _me._sendHitsBind();
-          Event.observe(window, 'beforeunload', _me._sendMissedHits.bind(_me));
-        }
-      }
+      _me._analyticsEventQueue = new Array();
+      _me._analyticsPriorizedEventQueue = new Array();
+      _me._addBind = _me.add.bind(_me);
+      _me._priorityAddBind = _me.priorityAdd.bind(_me);
+      _me._sizeBind = _me.size.bind(_me);
+      _me._lowPrioritySizeBind = _me.lowPrioritySize.bind(_me);
+      _me._highPrioritySizeBind = _me.highPrioritySize.bind(_me);
+      _me._sendHitsBind = _me._sendHits.bind(_me);
+      _me._getPrioMsgPartBind = _me._getPrioMsgPart.bind(_me);
+      _me._running = true;
+      _me._sendHitsBind();
+      Event.observe(window, 'beforeunload', _me._sendMissedHits.bind(_me));
     },
 
     add : function(action, parameters) {
       var _me = this;
       _me._analyticsEventQueue.push({ 'action' : action, 'params' : parameters });
-//      console.log('added event');
     },
 
     priorityAdd : function(action, parameters) {
       var _me = this;
       _me._analyticsPriorizedEventQueue.push({ 'action' : action, 'params' : parameters });
-//      console.log('added priorized event');
     },
 
     size : function() {
@@ -92,56 +75,21 @@
       var _me = this;
       return _me._analyticsPriorizedEventQueue.length;
     },
-
-    sentHitsCount : function() {
-      var _me = this;
-      if (typeof(gaData) !== 'undefined') {
-        return gaData[_me._analyticsId].hitcount;
-      }
-      return null;
-    },
-
+    
     _sendHits : function() {
       var _me = this;
-//      console.log('send hits, _running', _me._running, ', queue size is', _me._sizeBind(), ', hits availabe', _me._availableHits, ', hits sent', _me._sentHitsCountBind());
+      console.debug('send hits, _running', _me._running, ', queue size is', _me._sizeBind());
       if (_me._running) {
-        while ((_me._highPrioritySizeBind() > 0) && !_me._atHitsPerSessionLimitBind() && (_me._availableHits > 1)) { // always keep 1 in reserve (for out of hits or missed)
+        while (_me._highPrioritySizeBind() > 0) {
           var nextHit = _me._analyticsPriorizedEventQueue.shift();
-          ga(nextHit.action, nextHit.params);
-          _me._availableHits--;
+          window.CELEMENTS.analytics.MatomoQueue().push(['trackEvent', nextHit.params.eventAction, nextHit.params.eventLabel, nextHit.params.eventValue]);
         }
-        while ((_me._lowPrioritySizeBind() > 0) && !_me._atHitsPerSessionLimitBind() && (_me._availableHits > 2)) { // always keep 2 in reserve (+1 for a high prio event)
+        while (_me._lowPrioritySizeBind() > 0) {
           var nextHit = _me._analyticsEventQueue.shift();
-          ga(nextHit.action, nextHit.params);
-          _me._availableHits--;
+          window.CELEMENTS.analytics.MatomoQueue().push(['trackEvent', nextHit.params.eventAction, nextHit.params.eventLabel, nextHit.params.eventValue]);
         }
-        if (!_me._atHitsPerSessionLimitBind()) {
-          _me._availableHits = Math.min(_me._availableHits + 2, 20); //each second you gain 2 hits up to a max of 20
-          _me._sendHitsBind.delay(1.1); // +0.1 second to be sure hits are available
-        } else { // 2 hits remaining for session
-          if(_me._highPrioritySizeBind() > 0) {
-            var nextHit = _me._analyticsPriorizedEventQueue.shift();
-            ga(nextHit.action, nextHit.params);
-          }
-          _me._sendSessionOutOfHitsBind();
-        }
+        _me._sendHitsBind.delay(.5);
       }
-    },
-
-    _atHitsPerSessionLimit : function() {
-      var _me = this;
-      return _me._sentHitsCountBind() >= (_me.maxHitsPerSession - 2);
-    },
-
-    _sendSessionOutOfHits : function() {
-      var _me = this;
-      _me._running = false;
-      ga('send', {
-        'hitType': 'event',
-        'eventCategory': 'banner',
-        'eventAction': 'missed',
-        'eventLabel': _me._getPrioMsgPartBind() + _me._sizeBind() + ' in default queue on reaching 500 hits in session'
-      });
     },
 
     _sendMissedHits : function(event) {
@@ -149,12 +97,7 @@
       _me._sendHitsBind();
       _me._running = false;
       if ((_me._highPrioritySizeBind() + _me._sizeBind()) > 0) {
-        ga('send', {
-          'hitType': 'event',
-          'eventCategory': 'banner',
-          'eventAction': 'missed',
-          'eventLabel': _me._getPrioMsgPartBind() + _me._sizeBind() + ' in queue on unload'
-        });
+        window.CELEMENTS.analytics.MatomoQueue().push(['trackEvent', 'Ad', 'Missed hits: ', _me._getPrioMsgPartBind() + _me._sizeBind() + ' in queue on unload']);
       }
     },
 
@@ -169,12 +112,19 @@
       return prioMsg;
     }
   });
-
-  celAddOnBeforeLoadListener(function () {
-    if (window.ga) {
-      if (typeof window.CELEMENTS === "undefined"){ window.CELEMENTS={}; };
-      if (typeof window.CELEMENTS.analytics === "undefined"){ window.CELEMENTS.analytics={}; };
+  
+  const initQueue = function() {
+	if (window.CELEMENTS.analytics.MatomoQueue && window.CELEMENTS.analytics.MatomoQueue()) {
       window.CELEMENTS.analytics.Queue = new Queue();
-    }
+    } else {
+	  //console.debug('analytics delay', window.CELEMENTS.analytics.MatomoQueue);
+	  setTimeout(initQueue, 200);
+	}
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    if (typeof window.CELEMENTS === "undefined"){ window.CELEMENTS={}; };
+    if (typeof window.CELEMENTS.analytics === "undefined"){ window.CELEMENTS.analytics={}; };
+    initQueue();
   });
 })(window);
