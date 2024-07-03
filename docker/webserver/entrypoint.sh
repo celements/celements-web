6c86f02b346630d45ab8bfe5aba83f9b9db563b4
Marc Sladek <marc@sladek.dev>
2024-07-03 19:02:58 +0200

    updates

--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -5,7 +5,8 @@ echo "** installing hibernate.cfg.xml"
 envsubst < /usr/local/config/hibernate.cfg.xml \
   > ${CATALINA_HOME}/webapps/ROOT/WEB-INF/hibernate.cfg.xml
 
-# TODO xwiki.cfg
+# TODO xwiki.cfg - idea: custom envlocal profile in pom file 
+# with variables defined in local env file and being replaced here via envsubst
 
 echo "** installing log4j2.properties"
 envsubst < /usr/local/config/log4j2.properties \
