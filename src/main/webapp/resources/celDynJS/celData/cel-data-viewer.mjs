18aee45fc647c6a5df1f6ab413ccdcc8756862fc
Marc Sladek <marc@sladek.dev>
2024-12-09 11:26:58 +0100

    fix CelDataRegistry define

--- src/main/webapp/resources/celDynJS/celData/cel-data-viewer.mjs
+++ src/main/webapp/resources/celDynJS/celData/cel-data-viewer.mjs
@@ -1,8 +1,8 @@
 import pick from '/file/resource/deps/lodash/pick.js';
 import uniq from '/file/resource/deps/lodash/uniq.js';
-import CelDataRenderer from './cel-data-renderer.mjs?version=20240830';
+import CelDataRenderer from './cel-data-renderer.mjs?version=20241209';
 import CelDataLoader from './cel-data-loader.mjs?version=20240425';
-import { celDataRegistry } from './cel-data.mjs?version=20240830';
+import { celDataRegistry } from './cel-data.mjs?version=20241209';
 
 export class Config {
   tagName;
