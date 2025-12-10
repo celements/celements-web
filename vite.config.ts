a664eb7ae2fa2c714f77008ad4332f5fca63a62c
Marc Sladek <marc@sladek.me>
2025-12-10 23:32:17 +0100

    vite build / ts support (#496)
    * vite build
    
    * add sourcemap: true

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import input from './src/main/frontend/celements/index.ts';
+
+export default defineConfig({
+  build: {
+    outDir: 'src/main/webapp/resources/dist',
+    emptyOutDir: true,
+    manifest: '.vite/manifest.celements.json',
+    rollupOptions: {
+      input,
+      output: {
+        // Use .mjs extension for celements module detection
+        entryFileNames: '[name].[hash].mjs',
+        chunkFileNames: '[name].[hash].mjs',
+      }
+    },
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src/main/frontend', import.meta.url))
+    }
+  },
+});
