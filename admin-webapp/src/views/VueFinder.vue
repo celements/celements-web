461712d33636ab5bc504d33a69247c457c35f2ce
Fabian Pichler <fabian.pichler@synventis.com>
2026-01-06 01:03:22 +0100

    add celements-filebase dependency and improve VueFinder.vue

--- admin-webapp/src/views/VueFinder.vue
+++ admin-webapp/src/views/VueFinder.vue
@@ -18,7 +18,7 @@ import { RemoteDriver } from 'vuefinder';
 const logger=useLogger("VueFinder");
 
 const driver = new RemoteDriver({
-  baseURL: 'https://vuefinder-api.ozdemir.be/api/files',
+  baseURL: '/api/files',
 });
 
 logger.log('driver setup');
