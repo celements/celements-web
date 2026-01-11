0ba52fed57b1fd5fe3969fa53d2646ae85637f7d
Fabian Pichler <fabian.pichler@synventis.com>
2026-01-11 12:11:23 +0100

    rename VueFinder component to MediaLib

--- admin-webapp/src/views/MediaLib.vue
+++ admin-webapp/src/views/MediaLib.vue
@@ -31,19 +31,42 @@
         theme: false,
         pinned: false,
       }"
+      :custom-uploader="customUploader"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useLogger } from '@/utils/logger';
-import { RemoteDriver } from 'vuefinder';
+import { RemoteDriver, VueFinder } from 'vuefinder';
 
 const logger=useLogger("VueFinder");
 
 const driver = new RemoteDriver({
   baseURL: '/api/files',
 });
+
+const customUploader = (uppy: unknown) => {
+  const originalAddFile = uppy.addFile.bind(uppy);
+
+  uppy.addFile = (file: any) => {
+    console.log('[uppy addFile called]', file?.name ?? file);
+    console.trace('[uppy addFile stack]');
+    return originalAddFile(file);
+  };
+
+  window.__vfUppy = uppy;
+
+  uppy.on('file-added', (file: unknown) => console.log('[uppy file-added]', file?.name));
+  uppy.on('files-added', (files: unknown[]) => console.log('[uppy files-added]', files?.map(f => f.name)));
+  uppy.on('restriction-failed', (file: unknown, error: unknown) =>
+    console.warn('[uppy restriction-failed]', file?.name, error)
+  );
+
+  console.log('[uppy opts]', uppy.opts);
+  console.log('[uppy restrictions]', uppy.opts?.restrictions);
+  console.log('[uppy state]', uppy.getState?.());
+};
 
 logger.log('driver setup');
 </script>
