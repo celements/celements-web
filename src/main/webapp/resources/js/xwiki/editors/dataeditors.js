d7784a1c41d7b1a0b7d6ba05885247af6a227f1f
Chantal Pichler <chantal.pichler@synventis.com>
2026-03-12 17:38:59 +0100

    replace velocity $xwiki in Javascript

--- src/main/webapp/resources/js/xwiki/editors/dataeditors.js
+++ src/main/webapp/resources/js/xwiki/editors/dataeditors.js
@@ -152,7 +152,7 @@ XWiki.editors.XPropertyOrdering = Class.create({
     }
     $$(".xproperty-title").each(function (B) {
       var A = new Element("img", {
-        src: "$xwiki.getSkinFile('icons/datamodel/move.png', true)",
+        src: "/skin/resources/icons/datamodel/move.png",
         class: "move",
         alt: "move",
         title: "Drag and drop to change the order",
