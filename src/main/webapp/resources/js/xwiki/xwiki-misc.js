cba9f379e1969d694b1f96c6ed050af04154a26c
Chantal Pichler <chantal.pichler@synventis.com>
2026-03-16 13:39:57 +0100

    rewrite xwiki-misc.js without using prototype

--- src/main/webapp/resources/js/xwiki/xwiki-misc.js
+++ src/main/webapp/resources/js/xwiki/xwiki-misc.js
@@ -1,16 +1,20 @@
 /** this are js-snippets moved to disk from xwiki-sheets on
  * dropping skinx plugins **/
-document.observe('dom:loaded', function () {
-    var switcher = $('classname');
-    if (typeof (switcher) != 'undefined') {
-        switcher.observe('change', function () {
-            var value = this.options[this.selectedIndex].value;
-            if (value != '-') {
-                window.self.location = value;
-            }
-        }.bindAsEventListener(switcher));
-        switcher.up('form').down("input[type='submit']").hide();
+document.addEventListener("DOMContentLoaded", function () {
+  const switcher = document.getElementById("classname");
+  if (switcher) {
+    switcher.addEventListener("change", function () {
+      const value = this.options[this.selectedIndex].value;
+      if (value !== "-") {
+        window.location = value;
+      }
+    });
+    const form = switcher.closest("form");
+    if (form) {
+      const submit = form.querySelector("input[type='submit']");
+      if (submit) {
+        submit.style.display = "none";
+      }
     }
+  }
 });
-
-
