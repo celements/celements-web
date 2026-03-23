6951f6374fca6d00b758bbdedef52e2db1dec967
Fabian Pichler <fabian.pichler@synventis.com>
2026-03-23 02:44:35 +0100

    add PrimeVue and implement a select-favorite button

--- admin-webapp/src/views/MediaLib.vue
+++ admin-webapp/src/views/MediaLib.vue
@@ -31,42 +31,58 @@
         theme: false,
         pinned: false,
       }"
-      :custom-uploader="customUploader"
-    />
+      @select="handleSelect"
+      />
+      <!-- :custom-uploader="customUploader" -->
+    <Button v-if="selectedFiles.length > 0" label="set Favorite" @click="setFavorite()" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useLogger } from '@/utils/logger';
 import { RemoteDriver, VueFinder } from 'vuefinder';
+import Button from 'primevue/button';
+import { ref } from 'vue';
 
 const logger=useLogger("VueFinder");
+
+const selectedFiles = ref<any[]>([]);
+
+const handleSelect = (files: any[]) => {
+  selectedFiles.value = files;
+  logger.log('selected files', files);
+};
+
+const setFavorite = () => {
+  const file = selectedFiles.value[0];
+  logger.log('setFavorite', file);
+};
 
 const driver = new RemoteDriver({
   baseURL: '/api/files',
 });
 
-const customUploader = (uppy: unknown) => {
-  const originalAddFile = uppy.addFile.bind(uppy);
+// const customUploader = (uppy: any) => {
+//   const originalAddFile = uppy.addFile.bind(uppy);
 
-  uppy.addFile = (file: any) => {
-    console.log('[uppy addFile called]', file?.name ?? file);
-    console.trace('[uppy addFile stack]');
-    return originalAddFile(file);
-  };
+//   uppy.addFile = (file: any) => {
+//     logger.log('[uppy addFile called]', file?.name ?? file);
+//     logger.trace('[uppy addFile stack]');
+//     return originalAddFile(file);
+//   };
 
-  window.__vfUppy = uppy;
+//   window.__vfUppy = uppy;
 
-  uppy.on('file-added', (file: unknown) => console.log('[uppy file-added]', file?.name));
-  uppy.on('files-added', (files: unknown[]) => console.log('[uppy files-added]', files?.map(f => f.name)));
-  uppy.on('restriction-failed', (file: unknown, error: unknown) =>
-    console.warn('[uppy restriction-failed]', file?.name, error)
-  );
+//   uppy.on('file-added', (file: any) => logger.log('[uppy file-added]', file?.name));
+//   uppy.on('files-added', (files: any[]) => logger.log('[uppy files-added]', files?.map(f => f.name)));
+//   uppy.on('restriction-failed', (file: any, error: any) =>
+//     logger.warn('[uppy restriction-failed]', file?.name, error)
+//   );
 
-  console.log('[uppy opts]', uppy.opts);
-  console.log('[uppy restrictions]', uppy.opts?.restrictions);
-  console.log('[uppy state]', uppy.getState?.());
-};
+//   logger.log('[uppy opts]', uppy.opts);
+//   logger.log('[uppy restrictions]', uppy.opts?.restrictions);
+//   logger.log('[uppy state]', uppy.getState?.());
+// };
 
 logger.log('driver setup');
 </script>
