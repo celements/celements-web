9301230e07f31f174d198a3f2e724becae832edf
Fabian Pichler <fabian.pichler@synventis.com>
2026-05-15 09:34:54 +0200

    remove unused Home route

--- celements-admin-frontend/src/conf/routes/index.ts
+++ celements-admin-frontend/src/conf/routes/index.ts
@@ -1,25 +1,15 @@
-import Home from '@/views/Home.vue';
 import MediaLib from '@/views/MediaLib.vue';
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'home',
-    component: Home,
-    meta: {
-      title: 'Willkommen',
-//      requiresAuth: true,
-    },
-  },
-  {
     path: '/MediaLib',
     name: 'MediaLib',
     component: MediaLib,
     meta: {
-//      requiresAuth: true,
-    }
-  }
+      //      requiresAuth: true,
+    },
+  },
 ];
 
 export default routes;
