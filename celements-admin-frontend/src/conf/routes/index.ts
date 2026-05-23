31f1e4d29cfb9f3f23e26a28eadc76f227c085b2
Fabian Pichler <fabian.pichler@synventis.com>
2026-05-23 02:52:59 +0200

    add PageAttachments recursive app script and vue implementation

--- celements-admin-frontend/src/conf/routes/index.ts
+++ celements-admin-frontend/src/conf/routes/index.ts
@@ -1,11 +1,24 @@
 import MediaLib from '@/views/MediaLib.vue';
+import PageAttachments from '@/views/PageAttachments.vue';
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/',
+    redirect: '/MediaLib',
+  },
+  {
     path: '/MediaLib',
     name: 'MediaLib',
     component: MediaLib,
+    meta: {
+      //      requiresAuth: true,
+    },
+  },
+  {
+    path: '/PageAttachments/:spaceName/:docName',
+    name: 'PageAttachments',
+    component: PageAttachments,
     meta: {
       //      requiresAuth: true,
     },
